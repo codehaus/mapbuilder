f7e6f91024826e31ba304e67dbe1d72742590d4c
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2003-12-27 10:48:51 +0000

    Initial incomplete version

---
+++ mapbuilder/mapbuilder/lib/model/Context.js
@@ -0,0 +1,33 @@
+/*
+License: GPL as per: http://www.gnu.org/copyleft/gpl.html
+$Id$
+*/
+
+/**
+Stores a Web Map Context (WMC) document as defined by the Open GIS Consortium
+http://opengis.org and extensions the the WMC.  A unique Id is included for
+each layer which is used when referencing Dynamic HTML layers in MapPane.
+Dependancies: Sarissa (XML utilities).
+@constructor
+@version $Name$
+@author Cameron Shorter cameronATshorter.net
+*/
+function Context() {
+
+  /**
+   * The Web Map Context Document.  Make sure this is populated with setWmc().
+   */
+  var this.context = Sarissa.getDomDocument();
+  this.context.async = false;
+  // the following two lines are needed for IE
+  this.context.setProperty("SelectionNamespaces", "xmlns:xsl='http://www.w3.org/1999/XSL/Transform'");
+  this.context.setProperty("SelectionLanguage", "XPath");
+
+  /**
+   * Set a Wmc document from the given URL.
+   * @param url The URL of the WMC document.
+   */
+  this.setWmc = function(url) {
+    this.context.load(url);
+   }
+}
