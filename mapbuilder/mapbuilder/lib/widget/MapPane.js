c306880e532764114c5d4a1601aecd9c354a520c
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2003-12-30 20:31:06 +0000

    Pass in the baseDir, changed createLayers to paint

--- mapbuilder/mapbuilder/lib/widget/MapPane.js
+++ mapbuilder/mapbuilder/lib/widget/MapPane.js
@@ -10,20 +10,20 @@ $Id$
  * Tools to build a MapPane from a Web Map Context.
  * @constructor
  * @param context The Web Map Context to use when building this MapPane.
+ * @param baseDir The base mapbuilder lib directory.
  * @param window Not supported yet, writes to the current window.
  */
-function MapPane(context,window) {
+function MapPane(context,baseDir,window) {
   this.context=context;
   this.window=window;
-  this.wmcLayer2DhtmlLayer=new XslProcessor("../widget/mappane/WmcLayer2DhtmlLayer.xsl");
+  this.wmcLayer2DhtmlLayer=new XslProcessor(baseDir+"/mappane/WmcLayer2DhtmlLayer.xsl");
 
   /**
    * Create all the layers by rendering them into the current window.
    * This function should be called at startup.
    */
-  this.createLayers=function() {
-    s=this.wmcLayer2DhtmlLayer.transformNode(
-      this.context.context,this.window);
+  this.paint=function() {
+    s=this.wmcLayer2DhtmlLayer.transformNode(this.context.context);
     document.write(s);
   }
 }
