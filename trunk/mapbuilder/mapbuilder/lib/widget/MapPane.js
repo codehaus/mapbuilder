e935563bfb6e114bd2ae17d0a8e1c36e9b28067c
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2003-12-30 07:17:05 +0000

    initial version

---
+++ trunk/mapbuilder/mapbuilder/lib/widget/MapPane.js
@@ -0,0 +1,27 @@
+/*
+Author:       Cameron Shorter cameronAtshorter.net
+License:      GPL as per: http://www.gnu.org/copyleft/gpl.html
+Dependancies: Context, Sarissa, XslProcessor
+
+$Id$
+*/
+
+/**
+ * Tools to build a MapPane from a Web Map Context.
+ * @constructor
+ * @param context The Web Map Context to use when building this MapPane.
+ * @param window Not supported yet, writes to the current window.
+ */
+function MapPane(context,window) {
+  this.context=context;
+  this.window=window;
+  this.wmcLayer2DhtmlLayer=new XslProcessor("../widget/mappane/WmcLayer2DhtmlLayer.xsl");
+
+  /**
+   * Create all the layers by rendering them into the current window.
+   * This function should be called at startup.
+   */
+  this.createLayers=function() {
+    this.wmcLayer2DhtmlLayer.transformNode(this.context.context,this.window);
+  }
+}
