2877e834ed4786b21d241e47c1b4054686aba98c
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2003-12-30 07:13:09 +0000

    Fixed runtime errors

--- trunk/mapbuilder/mapbuilder/lib/model/Context.js
+++ trunk/mapbuilder/mapbuilder/lib/model/Context.js
@@ -9,19 +9,19 @@ http://opengis.org and extensions the the WMC.  A unique Id is included for
 each layer which is used when referencing Dynamic HTML layers in MapPane.
 Dependancies: Sarissa (XML utilities).
 @constructor
-@version $Name$
 @author Cameron Shorter cameronATshorter.net
 */
-function Context() {
+function Context(url) {
 
   /**
    * The Web Map Context Document.  Make sure this is populated with setWmc().
    */
-  var this.context = Sarissa.getDomDocument();
+  this.context = Sarissa.getDomDocument();
   this.context.async = false;
   // the following two lines are needed for IE
   this.context.setProperty("SelectionNamespaces", "xmlns:xsl='http://www.w3.org/1999/XSL/Transform'");
   this.context.setProperty("SelectionLanguage", "XPath");
+  this.context.load(url);
 
   /**
    * Set a Wmc document from the given URL.
