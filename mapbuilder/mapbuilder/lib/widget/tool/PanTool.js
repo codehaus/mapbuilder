bbcfdaef4e6b8f4db2b3fab082645c3db3c7a017
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2004-01-16 10:14:59 +0000

    Initial stub

---
+++ mapbuilder/mapbuilder/lib/widget/tool/PanTool.js
@@ -0,0 +1,28 @@
+/*
+License: GPL as per: http://www.gnu.org/copyleft/gpl.html
+Dependancies: Context
+$Id$
+*/
+
+/**
+ * Draws pan images and updates Context Bounds when a user clicks on the image.
+ * @constructor
+ * @author Nedjo
+ * @constructor
+ * @param context The Web Map Context to call when changing extent.
+ * @param baseDir The base mapbuilder lib directory.
+ * @param node Node from the HTML DOM to insert Pan HTML into.
+ */
+function PanTool(context,baseDir,node) {
+  this.context=context;
+  this.node=node;
+
+  /**
+   * Render the pan icons.
+   * This function should be called at startup.
+   */
+  this.paint=function(){
+    s="Insert Pan Functions here";
+    this.node.innerHTML=s;
+  }
+}
