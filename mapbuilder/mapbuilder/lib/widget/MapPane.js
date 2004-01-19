1264d56be052582afa4233af780ef5d7f243af75
nedjo <nedjo@78ec1247-0520-a048-8ee2-f9631b190d0c>
2004-01-19 19:29:03 +0000

    * Added move() and resize() methods * Added setClip() method to clip the containing div * Added moveImages() method to reposition map layer images   (e.g., during active panning) * Disabled xsl top and left parameter setting, as this is now handled by above methods

--- mapbuilder/mapbuilder/lib/widget/MapPane.js
+++ mapbuilder/mapbuilder/lib/widget/MapPane.js
@@ -21,25 +21,57 @@ function MapPane(context,baseDir,node) {
     this.wmcLayer2DhtmlLayer.xslDom,
     "baseDir", "'"+baseDir+"'");
 
+  this.move=function(left,top) {
+    this.node.style.left=left;
+    this.node.style.top=top;
+  }
+  this.resize=function(width,height) {
+    this.node.style.width=width;
+    this.node.style.height=height;
+  }
+
   /**
    * Create all the layers by rendering them into the current window.
    * This function should be called at startup.
    */
   this.paint=function() {
     // Set the coordinates of the mapImage for the xsl
-    Sarissa.setXslParameter(
-      this.wmcLayer2DhtmlLayer.xslDom,
-      "left",
-      String(getAbsX(this.node)-1));
-    Sarissa.setXslParameter(
-      this.wmcLayer2DhtmlLayer.xslDom,
-      "top",
-      String(getAbsY(this.node)-1));
+// These calls disabled here since shouldn't offset
+// from absolutely positioned containing div
+//    Sarissa.setXslParameter(
+//      this.wmcLayer2DhtmlLayer.xslDom,
+//      "left",
+//      String(getAbsX(this.node)-1));
+//    Sarissa.setXslParameter(
+//     this.wmcLayer2DhtmlLayer.xslDom,
+//      "top",
+//      String(getAbsY(this.node)-1));
 
+//  This method not yet functional--likely because XSL is returning incorrect
+//  object.
+
+//    result=this.wmcLayer2DhtmlLayer.transformNodeToObject(this.context.context);
+//    this.node.appendChild(result);
+
+//  So, in meantime, return string instead...
     s=this.wmcLayer2DhtmlLayer.transformNode(this.context.context);
     this.node.innerHTML=s;
-   }
 
+    this.setClip();
+   }
+   this.setClip=function(){
+     width=this.context.context.getWindowWidth();
+     height=this.context.context.getWindowHeight();
+     this.node.style.clip="rect(0," + width + "," + height + ",0)";
+   }
+   this.moveImages=function(left,top){
+    images=this.node.firstChild.getElementsByTagName("IMG");
+    for(i=0;i<images.length;i++){
+      img=images.item(i);
+      img.style.left=left;
+      img.style.top=top;
+    }
+   }
   /**
    * Called when the context's hidden attribute changes.
    * @param e The event sent to the listener.
