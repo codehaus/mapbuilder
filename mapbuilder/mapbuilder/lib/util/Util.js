fd44115cf7a1ca45679d6e53b37d3d5bbcb5474e
nedjo <nedjo@78ec1247-0520-a048-8ee2-f9631b190d0c>
2004-01-19 19:37:05 +0000

    * added getArgs function (for reading get-encoded args, e.g., a context doc url) * added getPageX and getPageY functions for reading event coordinates * roughed in getScreenX and getScreenY functions for converting geo to screen coords (not yet  usable)

--- mapbuilder/mapbuilder/lib/util/Util.js
+++ mapbuilder/mapbuilder/lib/util/Util.js
@@ -33,6 +33,12 @@ function XslProcessor(xslUrl) {
     s=a.join(">");
     return s;
   }
+  this.transformNodeToObject=function(xmlNode) {
+    // transform and build a web page with result
+    var result = Sarissa.getDomDocument();
+    xmlNode.transformNodeToObject(this.xslDom,result);
+    return result;
+  }
 }
 
 /**
@@ -57,12 +63,13 @@ function UniqueId(){
  */
 function getAbsX(elt) {
         return (elt.x) ? elt.x : getAbsPos(elt,"Left") + 2;
+}
+
 /**
  * get the absolute position of HTML element NS4, IE4/5 & NS6, even if it's in a table.
  * @param element The HTML element.
  * @return Top left Y position.
  */
-}
 function getAbsY(elt) {
         return (elt.y) ? elt.y : getAbsPos(elt,"Top") + 2;
 }
@@ -75,3 +82,67 @@ function getAbsPos(elt,which) {
  return iPos;
 }
 
+/**
+ * get the absolute position of a user event (e.g., a mousedown).
+ * @param e The user event.
+ * @return Left or top position.
+ */
+function getPageX(e){
+  var posx = 0;
+  if (!e) var e = window.event;
+  if (e.pageX) {
+    posx = e.pageX;
+  }
+  else if (e.clientX) {
+   posx = e.clientX;
+  }
+  return posx;
+}
+
+function getPageY(e){
+  var posy = 0;
+  if (!e) var e = window.event;
+  if (e.pageY) {
+    posy = e.pageY;
+  }
+  else if (e.clientY) {
+    posy = e.clientY;
+  }
+  return posy;
+}
+
+/*
+ * parse comma-separated name=value argument pairs from
+ * the query string of the URL; the function stores name=value
+ * pairs in properties of an object and returns that object. 
+*/
+function getArgs(){
+  var args = new Object();
+  var query = location.search.substring(1);   // Get query string.
+  var pairs = query.split("&");               // Break at ampersand.
+  for(var i = 0; i < pairs.length; i++) {
+    var pos = pairs[i].indexOf('=');          // Look for "name=value".
+    if (pos == -1) continue;                  // If not found, skip.
+    var argname = pairs[i].substring(0,pos);  // Extract the name.
+    var value = pairs[i].substring(pos+1);    // Extract the value.
+    args[argname] = decode(value);            // Store as a property.
+  }
+  return args;                                // Return the object.
+}
+
+// these two not yet usable, require mapWidth/mapHeight...
+function getScreenX(bbox,xCoord){
+  bbox[0]=parseFloat(bbox[0]);
+  bbox[2]=parseFloat(bbox[2]);
+  var xfac = (mapWidth/(bbox[2]-bbox[0]));
+  x=xfac*(xCoord-bbox[0]);
+  return x;
+}
+
+function getScreenY(yCoord){
+  bbox[1]=parseFloat(bbox[1]);
+  bbox[3]=parseFloat(bbox[3]);
+  var yfac = (mapHeight/(bbox[3]-bbox[1]));
+  pt.y=mapHeight-(yfac*(pt.y-bbox[1]));
+  return pt;
+}
