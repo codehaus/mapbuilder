39299bf3f97fb0683061539a2b93de6ea852c0f2
nedjo <nedjo@78ec1247-0520-a048-8ee2-f9631b190d0c>
2004-01-21 06:26:04 +0000

    first draft of zoom tool

---
+++ trunk/mapbuilder/mapbuilder/lib/widget/tool/ZoomTool.js
@@ -0,0 +1,76 @@
+/*
+
+License: GPL as per: http://www.gnu.org/copyleft/gpl.html
+
+Dependancies: Context
+
+$Id$
+
+*/
+
+
+
+/**
+
+ * Draws pan images and updates Context Bounds when a user clicks on the image.
+
+ * @constructor
+
+ * @author Nedjo
+
+ * @constructor
+
+ * @requires Context
+
+ * @param context The Web Map Context to call when changing extent.
+
+ * @param baseDir The base mapbuilder lib directory.
+
+ * @param node Node from the HTML DOM to insert Pan HTML into.
+
+ */
+
+function ZoomTool(context,baseDir) {
+
+  this.context=context;
+
+  var node=makeElt("DIV");
+
+  node.style.position="absolute";
+
+  this.node=node;
+
+  this.move=function(left,top) {
+
+    this.node.style.left=left;
+
+    this.node.style.top=top;
+
+  }
+
+  this.resize=function(width,height) {
+
+    this.node.style.width=width;
+
+    this.node.style.height=height;
+
+  }
+
+  /**
+
+   * Render the zoom icons.
+
+   * This function should be called at startup.
+
+   */
+
+  this.paint=function(){
+
+    s="<span onclick=\"context.zoom('in')\">Zoom in</span>";
+
+    this.node.innerHTML=s;
+
+  }
+
+}
+
