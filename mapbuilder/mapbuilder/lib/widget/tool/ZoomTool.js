1ffa084664816e2da1b8bde297aabb52a0851c8e
nedjo <nedjo@78ec1247-0520-a048-8ee2-f9631b190d0c>
2004-02-13 18:37:40 +0000

    Modified event handling to fix IE bug.

--- mapbuilder/mapbuilder/lib/widget/tool/ZoomTool.js
+++ mapbuilder/mapbuilder/lib/widget/tool/ZoomTool.js
@@ -1,118 +1,69 @@
 /*
-
 License: GPL as per: http://www.gnu.org/copyleft/gpl.html
-
 Dependancies: Context
-
 $Id$
-
 */
 
-
-
 /**
-
  * Draws pan images and updates Context Bounds when a user clicks on the image.
-
  * @constructor
-
  * @author Nedjo
-
  * @constructor
-
  * @requires Context
-
  * @param context The Web Map Context to call when changing extent.
-
  * @param node Node from the HTML DOM to insert Pan HTML into.
-
  */
-
 function ZoomTool(context, name, node) {
-
   this.context=context;
-
   this.name=name;
-
   if(node==null){
-
     node=makeElt("DIV");
-
     node.style.position="absolute";
-
   }
-
   this.node=node;
-
   this.move=function(left,top) {
-
     this.node.style.left=left;
-
     this.node.style.top=top;
-
   }
-
   this.resize=function(width,height) {
-
     this.node.style.width=width;
-
     this.node.style.height=height;
-
+  }
+  this.handleEvent=function(evt){
+    switch(returnTarget(evt).getAttribute("id")){
+      case 'zoomIn':
+        this.zoomIn(evt);
+        break;
+    }
   }
 
   /**
-
    * Zoom in (reset the BoundingBox).
-
    */
-
   this.zoomIn=function() {
-
     percent=20/100;
-
     bbox=this.context.getBoundingBox();
-
     geoWidth=parseFloat(bbox[2]-bbox[0]);
-
     geoHeight=parseFloat(bbox[1]-bbox[3]);
-
     bbox[0]=bbox[0]+(geoWidth*percent);
-
     bbox[2]=bbox[2]-(geoWidth*percent);
-
     bbox[1]=bbox[1]-(geoHeight*percent);
-
     bbox[3]=bbox[3]+(geoHeight*percent);
-
     this.context.setBoundingBox(bbox);
-
   }
-
   /**
-
    * Render the zoom icons.
-
    * This function should be called at startup.
-
    */
-
   this.paint=function(){
-
     img=document.createElement("img");
-
     img.style.cursor="pointer";
-
     img.setAttribute("src", this.context.skin+"images/zoomIn.gif");
-
-    // next line not working, so using setAttribute instead...
-
-    // img.addEventListener("click", eval(this.name+".zoomIn"), false);
-
-    img.setAttribute("onclick",this.name+".zoomIn();");
-
+    img.setAttribute("id", "zoomIn");
+    img.ownerObj=this;
+    addEvent(img, "click", handleEventWithObject);
     this.node.appendChild(img);
-
   }
-
 }
+
 
