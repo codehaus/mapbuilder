0fe696c711d4f6434e3ecfe1bee9c472469a6c4f
madair1 <madair1@78ec1247-0520-a048-8ee2-f9631b190d0c>
2004-03-07 17:50:11 +0000

    moved glasspane to mappane split Aoi box AOI and listeners in context

--- trunk/mapbuilder/mapbuilder/lib/widget/tool/AoiBox2.js
+++ trunk/mapbuilder/mapbuilder/lib/widget/tool/AoiBox2.js
@@ -20,11 +20,14 @@ function AoiBox(toolNode, parentWidget) {
   this.lineWidth = 2;                 // Zoombox line width; pass in as param?
   this.lineColor = "#FF0000";         // color of zoombox lines; pass in as param?
   this.crossSize = 9;
-  //this.mode = 'MODE_SET_AOI';   //temporary until mode gets implemented somewhere else?
-  this.mode = 'MODE_ZOOM_IN';   //temporary until mode gets implemented somewhere else?
 
-  this.node = document.getElementById( parentWidget.containerId );
+  this.model = parentWidget.model;
 
+  this.aoiListener = function(objRef) {
+    objRef.paint();
+  }
+
+  this.model.addAoiListener(this.aoiListener, this);
 
 /** Hide or show the box
   * @param vis    boolean true for visible; false for hidden
@@ -44,63 +47,33 @@ function AoiBox(toolNode, parentWidget) {
   * @param lr    lower right pixel/line coordinates
   * @return       none
   */
-  this.drawBox = function(ul, lr) {
-    this.ul = ul;
-    this.lr = lr;
-    this.paint();
-  }
-
-  this.mouseUpHandler = function( targetNode, objRef ) {
-    if (objRef.started) {
-      objRef.started = false;
-      var bbox = objRef.getBox();
-      var ul = targetNode.parentWidget.model.extent.GetXY( bbox[0] );
-      var lr = targetNode.parentWidget.model.extent.GetXY( bbox[1] );
-      switch(objRef.mode) {
-        case 'MODE_ZOOM_IN':				//zoom in
-          objRef.setVis(false);
-          var targetModel = targetNode.parentWidget.model;
-          if ( targetNode.parentWidget.targetGroup ) {
-            //add a listener to the targetGroup for updates to AOI box
-            targetModel = config[targetNode.parentWidget.targetGroup].model;
-          }
-          targetModel.extent.ZoomToBox( ul, lr );
-          break;
-        case 'MODE_SET_AOI':				//setting AOI
-          //call AOI changed listeners?; objRef.setFormAOI( ul, lr );
-        default:
-          //alert("invalid mode:" +objRef.mode);
-          break;
-      }
-    }
-  }
-
-  this.mouseDownHandler = function( targetNode, objRef ) {
-    objRef.start(targetNode.evpl);
-  }
-  this.mouseMoveHandler = function( targetNode, objRef ) {
-    if (objRef.started) objRef.dragBox(targetNode.evpl);
-  }
-
   this.paint = function() {
-    this.Top.style.left = this.ul[0];
-    this.Top.style.top = this.ul[1];
-    this.Top.style.width = this.lr[0]-this.ul[0]
+    var aoiBox = this.model.getAoi();
+    ul = aoiBox[0];
+    lr = aoiBox[1];
+    //check if ul=lr, then draw cross, else drawbox
+    this.drawBox(ul, lr);
+  }
+
+  this.drawBox = function(ul, lr) {
+    this.Top.style.left = ul[0];
+    this.Top.style.top = ul[1];
+    this.Top.style.width = lr[0]-ul[0]
     this.Top.style.height = this.lineWidth;
 
-    this.Left.style.left = this.ul[0];
-    this.Left.style.top = this.ul[1];
+    this.Left.style.left = ul[0];
+    this.Left.style.top = ul[1];
     this.Left.style.width = this.lineWidth;
-    this.Left.style.height = this.lr[1]-this.ul[1];
+    this.Left.style.height = lr[1]-ul[1];
 
-    this.Right.style.left = this.lr[0]-this.lineWidth;
-    this.Right.style.top = this.ul[1];
+    this.Right.style.left = lr[0]-this.lineWidth;
+    this.Right.style.top = ul[1];
     this.Right.style.width = this.lineWidth;
-    this.Right.style.height = this.lr[1]-this.ul[1];
+    this.Right.style.height = lr[1]-ul[1];
 
-    this.Bottom.style.left = this.ul[0];
-    this.Bottom.style.top = this.lr[1]-this.lineWidth;
-    this.Bottom.style.width = this.lr[0]-this.ul[0];
+    this.Bottom.style.left = ul[0];
+    this.Bottom.style.top = lr[1]-this.lineWidth;
+    this.Bottom.style.width = lr[0]-ul[0];
     this.Bottom.style.height = this.lineWidth;
 
     this.setVis(true);
@@ -115,14 +88,14 @@ function AoiBox(toolNode, parentWidget) {
     //this.ul = center;
     //this.lr = cen;
 
-    this.Top.style.left = Math.floor( (this.ul[0]+this.lr[0] - this.crossSize)/2);
-    this.Top.style.top = Math.floor( (this.ul[1]+this.lr[1] + this.crossSize)/2);
+    this.Top.style.left = Math.floor( center[0] - (this.crossSize/2) );
+    this.Top.style.top = Math.floor( center[1] + (this.crossSize/2) );
     this.Top.style.width = this.crossSize;
     this.Top.style.height = this.lineWidth;
     this.Top.style.visibility = "visible";
 
-    this.Left.style.left = Math.floor( (this.ul[0]+this.lr[0] - this.crossSize)/2);
-    this.Left.style.top = Math.floor( (this.ul[1]+this.lr[1] + this.crossSize)/2);
+    this.Left.style.left = Math.floor( center[0] - (this.crossSize/2) );
+    this.Left.style.top = Math.floor( center[1] + (this.crossSize/2) );
     this.Left.style.width = this.lineWidth;
     this.Left.style.height = this.crossSize;
     this.Left.style.visibility = "visible";
@@ -131,65 +104,6 @@ function AoiBox(toolNode, parentWidget) {
     this.Bottom.style.visibility = "hidden";
   }
 		
-/** called for starting a drag operation
-  * @param evpl    the coordinates to start the box at
-  * @return        none
-  */
-  this.start = function(evpl) {
-    this.anchorPoint = evpl;
-    this.dragBox( evpl );
-    this.started=true;
-  }
-
-/** called to start a drag operation
-  * @param vis    show or hide the box
-  * @return        none
-  */
-  this.stop = function(vis) {
-    this.started=false;
-    this.setVis(vis);
-    //alert(GetLatLonROI());
-  }
-
-/** Change the coordinate of one corner of the box.  The initial upper left 
-  * corner point stays fixed. 
-  * @param evpl    new corner coordinate
-  * @return        none
-  */
-  this.dragBox = function( evpl ) {	
-    var ul = new Array();
-    var lr = new Array();
-    if (this.anchorPoint[0] > evpl[0]) {
-      ul[0] = evpl[0];
-      lr[0] = this.anchorPoint[0];
-    } else {
-      ul[0] = this.anchorPoint[0];
-      lr[0] = evpl[0];
-    }
-    if (this.anchorPoint[1] > evpl[1]) {
-      ul[1] = evpl[1];
-      lr[1] = this.anchorPoint[1];
-    } else {
-      ul[1] = this.anchorPoint[1];
-      lr[1] = evpl[1];
-    }
-
-    this.drawBox( ul, lr );
-/*
-    if ( ( Math.abs(ul[0]-lr[0])<9 ) && ( Math.abs(ul[1]-lr[1])<9 ) ) {
-      this.drawCross( ul, lr );
-    } else {
-      this.drawBox( ul, lr );
-    }
-*/
-  }
-
-/** Returns an array of the corner coordinates as [ul, lr]
-  * @return        array of point arrays; ul=0, lr=1
-  */
-  this.getBox = function() {
-    return new Array(this.ul, this.lr);
-  }
 
 /** Internal method to initialize the box HTML elements
   */ 
@@ -198,45 +112,31 @@ function AoiBox(toolNode, parentWidget) {
     targetNode.evpl[1] = targetNode.evpl[1] + targetNode.offsetTop;
   }
 
-  this.getImageDiv = function( parentWidget ) {
+  this.getImageDiv = function( parentNode ) {
     var newDiv = document.createElement("DIV");
     newDiv.innerHTML = "&nbsp;";
     newDiv.style.position = "absolute";
     newDiv.style.backgroundColor = this.lineColor;
     //newDiv.style.zIndex = 300;
     newDiv.style.visibility = "hidden";
-
-/*
-    parentWidget.addMouseListener('mouseDown', this.mouseAddOffset, this );
-    parentWidget.addMouseListener('mouseMove', this.mouseAddOffset, this );
-    parentWidget.addMouseListener('mouseUp', this.mouseAddOffset, this );
-*/
-
-    this.node.appendChild( newDiv );
+    parentNode.appendChild( newDiv );
     return newDiv;
   }
 
   //final initialization
-  this.Top = this.getImageDiv( parentWidget );
-  this.Bottom = this.getImageDiv( parentWidget );
-  this.Left = this.getImageDiv( parentWidget );
-  this.Right = this.getImageDiv( parentWidget );
+  var containerNode = document.getElementById( parentWidget.containerId );
+  this.Top = this.getImageDiv( containerNode );
+  this.Bottom = this.getImageDiv( containerNode );
+  this.Left = this.getImageDiv( containerNode );
+  this.Right = this.getImageDiv( containerNode );
   this.ul = new Array(0,0);
   this.lr = new Array(0,0);
 
-  parentWidget.addMouseListener('mouseDown', this.mouseDownHandler, this );
-  parentWidget.addMouseListener('mouseMove', this.mouseMoveHandler, this );
-  parentWidget.addMouseListener('mouseUp', this.mouseUpHandler, this );
-
   //test case
-/*
   this.drawBox( new Array(2,2), new Array(200,200) );
-alert("AOIBox test");
+  alert("AOIBox test");
   this.drawCross( new Array(75,75) );
-alert("AOIBox test");
-  this.anchorPoint = new Array(75,75);
-  this.dragBox( new Array(300, 100) );
-*/
+  alert("AOIBox test");
 }
 
 
---
+++ trunk/mapbuilder/mapbuilder/lib/widget/tool/AoiMouseHandler.js
@@ -0,0 +1,77 @@
+/*
+Author:       Mike Adair mike.adairATccrs.nrcan.gc.ca
+License:      GPL as per: http://www.gnu.org/copyleft/gpl.html
+
+$Id$
+*/
+
+/**
+ * Tool to draw a region of interest box on a view.  The box can be drawn with
+ * the drawBox() method and can be tied to mouse drag with the dragBox() method.
+ * This object works entirely in pixel/line coordinate space and knows nothing
+ * about geography.
+ *
+ * @constructor
+ *
+ * @param viewNode    the view object node to attach the RoiBox to.
+ */
+
+function AoiMouseHandler(toolNode, parentWidget) {
+  this.model = parentWidget.model;
+
+  this.mouseUpHandler = function( targetNode, objRef ) {
+    if (objRef.started) objRef.started = false;
+  }
+
+  this.mouseDownHandler = function( targetNode, objRef ) {
+    objRef.start(targetNode.evpl);
+  }
+
+  this.mouseMoveHandler = function( targetNode, objRef ) {
+    if (objRef.started) objRef.dragBox(targetNode.evpl);
+  }
+
+/** called for starting a drag operation
+  * @param evpl    the coordinates to start the box at
+  * @return        none
+  */
+  this.start = function(evpl) {
+    this.anchorPoint = evpl;
+    this.dragBox( evpl );
+    this.started = true;
+  }
+
+/** Change the coordinate of one corner of the box.  The initial upper left 
+  * corner point stays fixed. 
+  * @param evpl    new corner coordinate
+  * @return        none
+  */
+  this.dragBox = function( evpl ) {	
+    var ul = new Array();
+    var lr = new Array();
+    if (this.anchorPoint[0] > evpl[0]) {
+      ul[0] = evpl[0];
+      lr[0] = this.anchorPoint[0];
+    } else {
+      ul[0] = this.anchorPoint[0];
+      lr[0] = evpl[0];
+    }
+    if (this.anchorPoint[1] > evpl[1]) {
+      ul[1] = evpl[1];
+      lr[1] = this.anchorPoint[1];
+    } else {
+      ul[1] = this.anchorPoint[1];
+      lr[1] = evpl[1];
+    }
+
+    //set new AOI in context
+    this.model.setAoi( ul, lr );
+  }
+
+
+  parentWidget.addMouseListener('mouseDown', this.mouseDownHandler, this );
+  parentWidget.addMouseListener('mouseMove', this.mouseMoveHandler, this );
+  parentWidget.addMouseListener('mouseUp', this.mouseUpHandler, this );
+}
+
+
