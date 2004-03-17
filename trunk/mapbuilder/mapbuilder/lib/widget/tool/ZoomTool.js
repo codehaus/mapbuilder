b7451842513f72adeff7d3e3275c6109bb48d116
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2004-03-17 20:06:10 +0000

    Moved ZoomTool3.js to ZoomTool.js, removed ZoomTool2.js

--- trunk/mapbuilder/mapbuilder/lib/widget/tool/ZoomTool.js
+++ trunk/mapbuilder/mapbuilder/lib/widget/tool/ZoomTool.js
@@ -13,57 +13,17 @@ $Id$
  * @param context The Web Map Context to call when changing extent.
  * @param node Node from the HTML DOM to insert Pan HTML into.
  */
-function ZoomTool(context, name, node) {
-  this.context=context;
-  this.name=name;
-  if(node==null){
-    node=makeElt("DIV");
-    node.style.position="absolute";
-  }
-  this.node=node;
-  this.move=function(left,top) {
-    this.node.style.left=left;
-    this.node.style.top=top;
-  }
-  this.resize=function(width,height) {
-    this.node.style.width=width;
-    this.node.style.height=height;
-  }
-  this.handleEvent=function(evt){
-    switch(returnTarget(evt).getAttribute("id")){
-      case 'zoomIn':
-        this.zoomIn(evt);
-        break;
-    }
+function ZoomInHandler(toolNode, parentWidget) {
+  this.parentWidget = parentWidget;
+  this.model = parentWidget.model;
+  this.modeValue = toolNode.selectSingleNode("modeValue").firstChild.nodeValue;
+
+  parentWidget[this.modeValue] = new Object();
+  parentWidget[this.modeValue].mouseup = function( model ) {
+    var bbox = model.getAoi();
+    var ul = model.extent.GetXY( bbox[0] );
+    var lr = model.extent.GetXY( bbox[1] );
+    model.extent.ZoomToBox( ul, lr );
   }
 
-  /**
-   * Zoom in (reset the BoundingBox).
-   */
-  this.zoomIn=function() {
-    percent=20/100;
-    bbox=this.context.getBoundingBox();
-    geoWidth=parseFloat(bbox[2]-bbox[0]);
-    geoHeight=parseFloat(bbox[1]-bbox[3]);
-    bbox[0]=bbox[0]+(geoWidth*percent);
-    bbox[2]=bbox[2]-(geoWidth*percent);
-    bbox[1]=bbox[1]-(geoHeight*percent);
-    bbox[3]=bbox[3]+(geoHeight*percent);
-    this.context.setBoundingBox(bbox);
-  }
-  /**
-   * Render the zoom icons.
-   * This function should be called at startup.
-   */
-  this.paint=function(){
-    img=document.createElement("img");
-    img.style.cursor="pointer";
-    img.setAttribute("src", this.context.skin+"images/zoomIn.gif");
-    img.setAttribute("id", "zoomIn");
-    img.ownerObj=this;
-    addEvent(img, "click", handleEventWithObject);
-    this.node.appendChild(img);
-  }
 }
-
-
