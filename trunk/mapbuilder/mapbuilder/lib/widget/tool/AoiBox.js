aa1c402a8eb477d1d2af97394c6b93e9436f30e3
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2004-03-17 20:03:05 +0000

    Moved AoiBox2 to AoiBox

--- trunk/mapbuilder/mapbuilder/lib/widget/tool/AoiBox.js
+++ trunk/mapbuilder/mapbuilder/lib/widget/tool/AoiBox.js
@@ -16,17 +16,13 @@ $Id$
  * @param viewNode    the view object node to attach the RoiBox to.
  */
 
-function RoiBox( viewNode ) {
-  this.lineWidth = 2;                 // Zoombox line width; pass in as param?
-  this.lineColor = "#FF0000";         // color of zoombox lines; pass in as param?
+function AoiBox(toolNode, parentWidget) {
+  this.lineWidth = toolNode.selectSingleNode("lineWidth").firstChild.nodeValue; // Zoombox line width; pass in as param?
+  this.lineColor = toolNode.selectSingleNode("lineColor").firstChild.nodeValue; // color of zoombox lines; pass in as param?
+  this.crossSize = toolNode.selectSingleNode("crossSize").firstChild.nodeValue;
 
-  this.Top = GetImageDiv( viewNode, this.lineColor );
-  this.Bottom = GetImageDiv( viewNode, this.lineColor );
-  this.Left = GetImageDiv( viewNode, this.lineColor );
-  this.Right = GetImageDiv( viewNode, this.lineColor );
-  this.ul = new Array(0,0);
-  this.lr = new Array(0,0);
-  this.crossSize = 9;
+  this.parentWidget = parentWidget;
+
 
 /** Hide or show the box
   * @param vis    boolean true for visible; false for hidden
@@ -46,28 +42,40 @@ function RoiBox( viewNode ) {
   * @param lr    lower right pixel/line coordinates
   * @return       none
   */
-  this.drawBox = function(ul, lr) {
-    this.ul = ul;
-    this.lr = lr;
+  this.paint = function() {
+    var aoiBox = this.parentWidget.model.getAoi();
+    ul = aoiBox[0];
+    lr = aoiBox[1];
+    //check if ul=lr, then draw cross, else drawbox
+//alert(    Math.abs( ul[0]-lr[0] ) + ":" + Math.abs( ul[1]-lr[1] ) );
 
-    this.Top.style.left = this.ul[0];
-    this.Top.style.top = this.ul[1];
-    this.Top.style.width = this.lr[0]-this.ul[0]
+    if ( (Math.abs( ul[0]-lr[0] ) < this.crossSize) && 
+        (Math.abs( ul[1]-lr[1] ) < this.crossSize) ) {
+      this.drawCross( new Array( (ul[0]+lr[0])/2, (ul[1]+lr[1])/2) );
+    } else {
+      this.drawBox(ul, lr);
+    }
+  }
+
+  this.drawBox = function(ul, lr) {
+    this.Top.style.left = ul[0];
+    this.Top.style.top = ul[1];
+    this.Top.style.width = lr[0]-ul[0]
     this.Top.style.height = this.lineWidth;
 
-    this.Left.style.left = this.ul[0];
-    this.Left.style.top = this.ul[1];
+    this.Left.style.left = ul[0];
+    this.Left.style.top = ul[1];
     this.Left.style.width = this.lineWidth;
-    this.Left.style.height = this.lr[1]-this.ul[1];
+    this.Left.style.height = lr[1]-ul[1];
 
-    this.Right.style.left = this.lr[0]-this.lineWidth;
-    this.Right.style.top = this.ul[1];
+    this.Right.style.left = lr[0]-this.lineWidth;
+    this.Right.style.top = ul[1];
     this.Right.style.width = this.lineWidth;
-    this.Right.style.height = this.lr[1]-this.ul[1];
+    this.Right.style.height = lr[1]-ul[1];
 
-    this.Bottom.style.left = this.ul[0];
-    this.Bottom.style.top = this.lr[1]-this.lineWidth;
-    this.Bottom.style.width = this.lr[0]-this.ul[0];
+    this.Bottom.style.left = ul[0];
+    this.Bottom.style.top = lr[1]-this.lineWidth;
+    this.Bottom.style.width = lr[0]-ul[0];
     this.Bottom.style.height = this.lineWidth;
 
     this.setVis(true);
@@ -78,18 +86,18 @@ function RoiBox( viewNode ) {
   * @param lr    lower right pixel/line coordinates
   * @return       none
   */
-  this.drawCross = function(ul, lr) {
-    this.ul = ul;
-    this.lr = lr;
+  this.drawCross = function(center) {
+    //this.ul = center;
+    //this.lr = cen;
 
-    this.Top.style.left = Math.floor( (this.ul[0]+this.lr[0] - this.crossSize)/2);
-    this.Top.style.top = Math.floor( (this.ul[1]+this.lr[1] + this.crossSize)/2);
+    this.Top.style.left = Math.floor( center[0] - this.crossSize/2 );
+    this.Top.style.top = Math.floor( center[1] - this.lineWidth/2 );
     this.Top.style.width = this.crossSize;
     this.Top.style.height = this.lineWidth;
     this.Top.style.visibility = "visible";
 
-    this.Left.style.left = Math.floor( (this.ul[0]+this.lr[0] - this.crossSize)/2);
-    this.Left.style.top = Math.floor( (this.ul[1]+this.lr[1] + this.crossSize)/2);
+    this.Left.style.left = Math.floor( center[0] - this.lineWidth/2 );
+    this.Left.style.top = Math.floor( center[1] - this.crossSize/2 );
     this.Left.style.width = this.lineWidth;
     this.Left.style.height = this.crossSize;
     this.Left.style.visibility = "visible";
@@ -98,98 +106,51 @@ function RoiBox( viewNode ) {
     this.Bottom.style.visibility = "hidden";
   }
 		
-/** called for starting a drag operation
-  * @param evpl    the coordinates to start the box at
-  * @return        none
-  */
-  this.start = function(evpl) {
-    if (this.started) {
-      this.stop(true);
-    } else {
-      this.anchorPoint = evpl;
-      this.dragBox( evpl );
-      this.started=true;
-    }
-  }
-
-/** called to start a drag operation
-  * @param vis    show or hide the box
-  * @return        none
-  */
-  this.stop = function(vis) {
-    this.started=false;
-    this.setVis(vis);
-    //alert(GetLatLonROI());
-  }
-
-/** Change the coordinate of one corner of the box.  The initial upper left 
-  * corner point stays fixed. 
-  * @param evpl    new corner coordinate
-  * @return        none
-  */
-  this.dragBox = function( evpl ) {	
-//if (evpl[0]==0) alert(evpl[0]+":"+evpl[1]);
-    var ul = new Array();
-    var lr = new Array();
-    if (this.anchorPoint[0] > evpl[0]) {
-      ul[0] = evpl[0];
-      lr[0] = this.anchorPoint[0];
-    } else {
-      ul[0] = this.anchorPoint[0];
-      lr[0] = evpl[0];
-    }
-    if (this.anchorPoint[1] > evpl[1]) {
-      ul[1] = evpl[1];
-      lr[1] = this.anchorPoint[1];
-    } else {
-      ul[1] = this.anchorPoint[1];
-      lr[1] = evpl[1];
-    }
-
-    this.drawBox( ul, lr );
-/*
-    if ( ( Math.abs(ul[0]-lr[0])<9 ) && ( Math.abs(ul[1]-lr[1])<9 ) ) {
-      this.drawCross( ul, lr );
-    } else {
-      this.drawBox( ul, lr );
-    }
-*/
-  }
-
-/** Returns an array of the corner coordinates as [ul, lr]
-  * @return        array of point arrays; ul=0, lr=1
-  */
-  this.getBox = function() {
-    return new Array(this.ul, this.lr);
-  }
-
-
-  //test case
-/*
-  this.drawBox( new Array(50,50), new Array(200,200) );
-  this.dragBox( new Array(300, 100) );
-*/
-}
 
 /** Internal method to initialize the box HTML elements
-  */
-function GetImageDiv( viewNode, lineColor ) {
-  var newDiv = document.createElement("DIV");
-  newDiv.innerHTML = "<IMG SRC='' WIDTH='1' HEIGHT='1'/>";
-  newDiv.style.position = "absolute";
-  newDiv.style.backgroundColor = lineColor;
-//  newDiv.style.zIndex = viewNode.style.zIndex;
-  newDiv.style.zIndex = 300;
-  newDiv.style.visibility = "hidden";
+  */ 
+  this.mouseAddOffset = function( ev ) {
+    targetNode.evpl[0] = targetNode.evpl[0] + targetNode.offsetLeft;
+    targetNode.evpl[1] = targetNode.evpl[1] + targetNode.offsetTop;
+  }
 
-  //let mouse events bubble up to the containing DIV
-  newDiv.onmousedown = function(ev) {;};// {alert(ev.layerX+":"+ev.layerY);};
-  newDiv.onmouseup = function(ev) {;};// {alert(ev.layerX+":"+ev.layerY);};
-  newDiv.onmousemove = function(ev) {;};// {alert(ev.layerX+":"+ev.layerY);};
-  newDiv.onmouseover = function(ev) {;};// {alert(ev.layerX+":"+ev.layerY);};
-  newDiv.onmouseout = function(ev) {;};// {alert(ev.layerX+":"+ev.layerY);};
+  this.getImageDiv = function( parentNode ) {
+    var newDiv = document.createElement("DIV");
+    newDiv.innerHTML = "&nbsp;";
+    newDiv.style.position = "absolute";
+    newDiv.style.backgroundColor = this.lineColor;
+    //newDiv.style.zIndex = 300;
+    newDiv.style.visibility = "hidden";
+//newDiv.onmousemove = this.mouseAddOffset;
+//newDiv.onmouseover = this.mouseAddOffset;
+    parentNode.appendChild( newDiv );
+    return newDiv;
+  }
 
-  viewNode.appendChild( newDiv );
-  return newDiv;
+  this.aoiListener = function(objRef) {
+    objRef.paint();
+  }
+
+  //final initialization
+  this.loadAoiBox = function( objRef ) {
+    var containerNode = document.getElementById( objRef.parentWidget.containerId );
+    objRef.Top = objRef.getImageDiv( containerNode );
+    objRef.Bottom = objRef.getImageDiv( containerNode );
+    objRef.Left = objRef.getImageDiv( containerNode );
+    objRef.Right = objRef.getImageDiv( containerNode );
+
+    //test case
+/*
+    objRef.drawBox( new Array(2,2), new Array(200,200) );
+    alert("AOIBox test");
+    objRef.drawCross( new Array(60,100) );
+    alert("AOIBox test");
+*/
+  }
+
+  this.parentWidget.model.addAoiListener(this.aoiListener, this);
+  this.parentWidget.addPaintListener( this.loadAoiBox, this );
+
 }
+
 
