b8a83088c51693b3a860f2ded6e39ddea508ff45
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2004-03-18 19:49:17 +0000

    Reformatting

--- mapbuilder/mapbuilder/lib/widget/WidgetBase.js
+++ mapbuilder/mapbuilder/lib/widget/WidgetBase.js
@@ -18,88 +18,83 @@ $Id$
 function WidgetBase(widgetNode, group) {
   if ( arguments.length > 0 ) {   //need this for .prototype?
 
-  this.id = widgetNode.attributes.getNamedItem("id").nodeValue;
-  this.node = document.getElementById( this.id );
-  if( this.node==null ) {
-    alert("HTML node for " + widgetNode.nodeName + " not found: id:" + this.id);
-  }
-  this.parentGroup = group;
-  this.model = group.model;
-  this.widgetNode = widgetNode;
+    this.id = widgetNode.attributes.getNamedItem("id").nodeValue;
+    this.node = document.getElementById( this.id );
+    if( this.node==null ) {
+      alert("HTML node for " + widgetNode.nodeName + " not found: id:" + this.id);
+    }
+    this.parentGroup = group;
+    this.model = group.model;
+    this.widgetNode = widgetNode;
 
-  var styleNode = widgetNode.selectSingleNode("stylesheet");
-  if ( styleNode ) this.stylesheet = new XslProcessor( config.baseDir + styleNode.firstChild.nodeValue );
+    var styleNode = widgetNode.selectSingleNode("stylesheet");
+    if ( styleNode ) this.stylesheet = new XslProcessor( config.baseDir + styleNode.firstChild.nodeValue );
 
-  var targetGroup = widgetNode.selectSingleNode("targetWidgetGroup");
-  if ( targetGroup ) this.targetGroup = targetGroup.firstChild.nodeValue;
+    var targetGroup = widgetNode.selectSingleNode("targetWidgetGroup");
+    if ( targetGroup ) this.targetGroup = targetGroup.firstChild.nodeValue;
 
 
-  /**
-   * Move this widget to the absolute (left,top) position in the browser.
-   * @param left Absolute left coordinate.
-   * @param top Absolute top coordinate.
-   */
-  this.move = function(left,top) {
-    this.node.style.left = left;
-    this.node.style.top = top;
-  }
-
-  /**
-   * Resize this widget.
-   * @param width New width.
-   * @param height New height.
-   */
-  this.resize = function(width,height) {
-    this.node.style.width = width;
-    this.node.style.height = height;
-  }
-
-  /**
-   * Render the widget
-   */
-  this.paint = function() {
-    this.stylesheet.setParameter("modelIndex", this.model.modelIndex );
-    var s = this.stylesheet.transformNode(this.model.doc);
-    this.node.innerHTML = s;
-
-    for (var i=0; i<this.paintListeners.length; i++) {
-      this.paintListeners[i][0]( this.paintListeners[i][1] );
+    /**
+     * Move this widget to the absolute (left,top) position in the browser.
+     * @param left Absolute left coordinate.
+     * @param top Absolute top coordinate.
+     */
+    this.move = function(left,top) {
+      this.node.style.left = left;
+      this.node.style.top = top;
     }
 
-  }
-
-  /**
-   * load all the child tools of this widget
-   */
-  this.loadTools = function() {
-    var tools = this.widgetNode.selectNodes( "tools/*" );
-    for (var i=0; i<tools.length; i++ ) {
-      var toolNode = tools[i];
-      evalStr = "new " + toolNode.nodeName + "(toolNode, this);";
-      //alert("Config.loadWidgets eval:" + evalStr);
-      this[toolNode.nodeName] = eval( evalStr );
+    /**
+     * Resize this widget.
+     * @param width New width.
+     * @param height New height.
+     */
+    this.resize = function(width,height) {
+      this.node.style.width = width;
+      this.node.style.height = height;
     }
+
+    /**
+     * Render the widget.
+     */
+    this.paint = function() {
+      this.stylesheet.setParameter("modelIndex", this.model.modelIndex );
+      var s = this.stylesheet.transformNode(this.model.doc);
+      this.node.innerHTML = s;
+
+      for (var i=0; i<this.paintListeners.length; i++) {
+        this.paintListeners[i][0]( this.paintListeners[i][1] );
+      }
+    }
+
+    /**
+     * load all the child tools of this widget.
+     */
+    this.loadTools = function() {
+      var tools = this.widgetNode.selectNodes( "tools/*" );
+      for (var i=0; i<tools.length; i++ ) {
+        var toolNode = tools[i];
+        evalStr = "new " + toolNode.nodeName + "(toolNode, this);";
+        this[toolNode.nodeName] = eval( evalStr );
+      }
+    }
+
+    /** Functions to call when the widget is painted */
+    this.paintListeners = new Array();
+
+    /**
+     * Add a Listener for AoiBox change.
+     * @param listener The function to call when the Area Of Interest changes.
+     * @param target The object which owns the listener function.
+     */
+    this.addPaintListener = function(listener,target) {
+      this.paintListeners[this.paintListeners.length] = new Array(listener,target);
+    }
+
+    /**
+     * no-op function; override in derived classes to add listeners to the model.
+     */
+    this.addListeners = function() {}
+
   }
-
-  /** Functions to call when the widget is painted */
-  this.paintListeners = new Array();
-
-  /**
-   * Add a Listener for AoiBox change.
-   * @param listener The function to call when the Area Of Interest changes.
-   * @param target The object which owns the listener function.
-   */
-  this.addPaintListener = function(listener,target) {
-    this.paintListeners[this.paintListeners.length] = new Array(listener,target);
-  }
-
-
-
-  /**
-   * no-op function; override in derived classes to add listeners to the model
-   */
-  this.addListeners = function() {}
-
-  }
-
 }
