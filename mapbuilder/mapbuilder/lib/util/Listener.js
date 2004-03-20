40d92a5936656f859ca488c27817d68a474a7f91
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2004-03-20 10:18:03 +0000

    Tested and removed some bugs

--- mapbuilder/mapbuilder/lib/util/Listener.js
+++ mapbuilder/mapbuilder/lib/util/Listener.js
@@ -10,48 +10,63 @@ $Id$
  * @author Cameron Shorter
  */
 function Listener() {
-  /** An array [variables] of (listener, target). */
+  /** An array [params] of (listener, target). */
   this.listeners=new Array();
 
   /**
-   * An array [variables] of values. These values might not be used if the set/get
+   * An array [params] of values. These values might not be used if the set/get
    * functions are overwritten.
    */
   this.values=new Array();
 
   /**
-   * Add a listener function which will be called when variable is updated;  The
+   * Add a listener function which will be called when param is updated;  The
    * listener function should be of the form: function listener(target) {..}
    * @param listener The function to call when the Area Of Interest changes.
    * @param target The object which owns the listener function.
-   * @param variable Variable name; if this variable changes then an event is
+   * @param param Parameter name; if this parameter changes then an event is
    * sent to all interested listeners.
    */
-  this.addListener(variable, listener, target) {
-    this.listeners[variable].push(new Array(listener,target));
+  this.addListener=function(param, listener, target) {
+    if(!this.listeners[param]){
+      this.listeners[param]=new Array();
+    }
+    this.listeners[param].push(new Array(listener,target));
   }
 
   /**
-   * Update variable and call all interested listeners.  This function may
-   * be overloaded to store the variable in a elsewhere (eg in an XML
-   * document).
+   * Call all the listeners that have registered interest in this parameter
+   * using addListener.
+   * @param param The parameter to change.
    */
-  this.setVariable(variable,value) {
-    this.variables[variable] = value;
-
-  // Call all the interested listeners
-  if (this.listeners[variable]){
-    for(var i=0;i<this.listeners[variable].length;i++){
-      this.listeners[variable][i][0](this.listeners[i][1]);
+  this.callListeners=function(param) {
+    if (this.listeners[param]){
+      for(var i=0;i<this.listeners[param].length;i++){
+        this.listeners[param][i][0](this.listeners[param][i][1]);
+      }
     }
   }
 
   /**
-   * Return the variable value, or return null if it does not exist.  This
-   * function may be overloaded to store the variable elsewhere (eg in
+   * Update parameter and call all interested listeners.  This function may
+   * be overloaded to store the parameter in a elsewhere (eg in an XML
+   * document).
+   * @param param The parameter to change.
+   * @parma value The new value of the param.
+   */
+  this.setParam=function(param,value) {
+    this.params[param] = value;
+
+    // Call all the interested listeners
+    this.callListeners(param);
+  }
+
+  /**
+   * Return the param value, or return null if it does not exist.  This
+   * function may be overloaded to store the param elsewhere (eg in
    * an XML document).
    */
-  this.getVariable(variable) {
-    return this.variables[variable][0];
+  this.getParam=function(param) {
+    return this.params[param][0];
   }
 }
