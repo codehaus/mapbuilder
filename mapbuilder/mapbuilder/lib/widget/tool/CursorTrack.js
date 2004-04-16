af357fce5ec0f0e1fecf81c1f8cc585e2acb86be
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2004-04-16 10:37:00 +0000

    Revert to version 1.43 - lat/long tracking is not working

--- mapbuilder/mapbuilder/lib/widget/tool/CursorTrack.js
+++ mapbuilder/mapbuilder/lib/widget/tool/CursorTrack.js
@@ -22,16 +22,12 @@ function CursorTrack(toolNode, parentWidget) {
   var formName = toolNode.selectSingleNode("formName").firstChild.nodeValue;
   this.coordForm = document.getElementById(formName);
 
-  /**
-   * Process a mouse action.
-   * set an interval to output the coords so that it doesn't execute on every
-   * move event.  The setInterval method in IE doesn't allow passing of an
-   * argument to the function called so set a global reference to glass pane
-   * here;  mouse can only be over one glass pane at time so this should be safe.
-   * @param objRef Pointer to this CurorTrack object.
-   * @param targetNode The node for the enclosing HTML tag for this widget.
-   */
-  this.mouseOverHandler = function(objRef,targetNode) {
+
+  this.mouseOverHandler = function( targetNode, objRef ) {
+    // set an interval to output the coords so that it doesn't execute on every move event.
+    // the setInterval method in IE doesn't allow passing of an argument to the function called
+    // so set a global reference to glass pane here;  mouse can only be over one glass pane at 
+    // time so this should be safe
     window.cursorTrackObject = objRef;
     window.cursorTrackNode = targetNode;
     objRef.mouseTrackTimer = setInterval( ReportCoords, 100, objRef);
@@ -41,7 +37,7 @@ function CursorTrack(toolNode, parentWidget) {
     if (objRef.mouseTrackTimer) clearInterval(objRef.mouseTrackTimer);
   }
 
-  parentWidget.addListener('mouseover', this.mouseOverHandler, this );
+  parentWidget.addMouseListener('mouseOver', this.mouseOverHandler, this );
   parentWidget.addMouseListener('mouseOut', this.mouseOutHandler, this );
 }
 
