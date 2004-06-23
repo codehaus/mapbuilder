d2c24482457bac8854cb92f9fb7edcf82a8aadd1
tomkralidis <tomkralidis@78ec1247-0520-a048-8ee2-f9631b190d0c>
2004-06-23 22:17:42 +0000

    Modified to reflect present design approach (it's been awhile)

--- mapbuilder/mapbuilder/lib/widget/Locations.js
+++ mapbuilder/mapbuilder/lib/widget/Locations.js
@@ -1,53 +1,37 @@
 /*
-License: GPL as per: http://www.gnu.org/copyleft/gpl.html
+Author:       Tom Kralidis
+License:      GPL as per: http://www.gnu.org/copyleft/gpl.html
+
 $Id$
 */
 
 // Ensure this object's dependancies are loaded.
-Mapbuilder.loadScript(baseDir+"/widget/WidgetBase.js");
+mapbuilder.loadScript(baseDir+"/widget/WidgetBase.js");
 
 /**
- * Functions to render a Predefined Locations select box from a GML document
- * with locations and their coordinates 
+ * Widget to display predefined locations
+ *
  * @constructor
- * @author Tom Kralidis tom.kralidis_at_ec.gc.ca
- * @constructor
- * @requires Sarissa
- * @requires Util
- * @requires Context
- * @param context The Web Map Context which contains the state of this legend.
- * @param name Variable name referencing this Legend object
- * @param node Node from the HTML DOM to insert legend HTML into.
+ *
+ * @param widgetNode This widget's object node from the configuration document.
+ * @param model The model that this widget is a view of.
  */
 
-function Locations(context, name, node) {
-  this.context=context;
-  this.name=name;
-  if(node==null){
-    node=makeElt("DIV");
-    node.style.position="absolute";
-    node.style.overflow="auto";
+
+function Locations(widgetNode, model) {
+  var base = new WidgetBase(widgetNode, model);
+  for (sProperty in base) {
+    this[sProperty] = base[sProperty];
   }
-  this.node=node;
-  this.locations2Select = new XslProcessor(baseDir+"/widget/locations/Locations2Select.xsl");
 
   /**
-   * Render the predefined locations into select box.
-   * This function should be called at startup.
+   * Change the AOI coordinates from select box choice of prefined locations
+   * @param bbox the bbox value of the location keyword chosen
    */
-  this.paint=function(){
-    s=this.locations2Select.transformNode(this.context.context);
-    this.node.innerHTML=s;
+
+  this.setAoi = function(bbox) {
+    var bboxArray = new Array();
+    bboxArray     = bbox.split(",");
+    config.setParam("aoi", new Array(new Array(bboxArray[0],bboxArray[3]), new Array(bboxArray[2], bboxArray[1])));
   }
-
-
-  /**
-   * Called when the select box selectedIndex changes
-   * @param e The event sent to the listener.
-   */
-  this.hiddenListener=function(e){
-    //context.setBoundingBox("40 40 40 40bbox);
-  }
-
-  this.context.addHiddenListener(this.hiddenListener);
 }
