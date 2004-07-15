6a2c67cc8390f61f481b406fdc1a574ae14d9840
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2004-07-15 01:25:18 +0000

    Initial version

---
+++ mapbuilder/mapbuilder/lib/widget/FeatureList.js
@@ -0,0 +1,32 @@
+/*
+License: GPL as per: http://www.gnu.org/copyleft/gpl.html
+$Id$
+*/
+
+// Ensure this object's dependancies are loaded.
+mapbuilder.loadScript(baseDir+"/widget/WidgetBase.js");
+
+/**
+ * Functions to render and update a FeatureList from GML.
+ * This widget extends WidgetBase.
+ * @constructor
+ * @author Cameron Shorter cameronATshorter.net
+ * @param widgetNode  The widget's XML object node from the configuration document.
+ * @param model       The model object that this widget belongs to.
+ */
+function FeatureList(widgetNode, model) {
+  var base = new WidgetBase(widgetNode, model);
+  for (sProperty in base) { 
+    this[sProperty] = base[sProperty]; 
+  } 
+
+  /**
+   * Render this widget.
+   * @param widget This widget object.
+   */
+  this.loadModelListener=function(widget){
+    widget.paint();
+  }
+
+  this.model.addListener("loadModel",this.loadModelListener,this);
+}
