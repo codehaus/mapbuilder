b82d7c3091bd29e87b86d1e3e95ebbfed25c8639
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2004-08-09 10:02:43 +0000

    Draw a cross/box based on AOI. Drawing the box has been commented out

--- mapbuilder/mapbuilder/lib/widget/AoiBox2.js
+++ mapbuilder/mapbuilder/lib/widget/AoiBox2.js
@@ -27,22 +27,55 @@ function AoiBox2(widgetNode, model) {
 
   this.node.style.position="absolute";
   this.node.visibility="hidden";
-  this.node.style.height="250px";
-  //this.node.style.top=this.node.parentNode.offsetTop;
-  //this.node.style.left=this.node.parentNode.offsetLeft;
-  //alert("node.parent.parent="+this.node.parentNode.parentNode.xml);
 
   /** WZ Graphics object and rendering functions. */
   this.jg=new jsGraphics(this.node.id);
   this.jg.setColor(this.lineColor);
+  this.jg.setColor("#00FF00");
+  //TBD: The following causes lines to be drawn incorrectly in Mozilla 1.71
   //this.jg.setStroke(this.lineWidth);
 
   /**
    * Render the widget.
+   * If the box width or height is less than the cross size, then draw a cross,
+   * otherwise draw a box.
    * @param objRef Pointer to this object.
    */
   this.paint = function(objRef) {
-    objRef.jg.drawRect(0,0,120,55);
-    objRef.jg.paint();
+    aoiBox = this.model.getParam("aoi");
+    if (aoiBox) {
+      ul = this.model.extent.GetPL(aoiBox[0]);
+      lr = this.model.extent.GetPL(aoiBox[1]);
+      width= lr[0]-ul[0];
+      height= lr[1]-ul[1];
+
+      objRef.jg.clear();
+
+      //check if ul=lr, then draw cross, else drawbox
+      if ((width < this.crossSize) && (height < this.crossSize) ) {
+        // draw cross
+        x=(lr[0]+ul[0])/2;
+        y=(lr[1]+ul[1])/2;
+        c=objRef.crossSize/2;
+        objRef.jg.drawLine(x+c,y,x-c,y);
+        objRef.jg.drawLine(x,y+c,x,y-c);
+      } else {
+        // draw box
+        //TBD the following line seems to disable the mouseup event in Mozilla
+        //objRef.jg.drawRect(ul[0],ul[1],width,height);
+      }
+      objRef.jg.paint();
+    }
   }
+ 
+  /**
+   * Called when the AoiChanged.
+   * @param objRef This object.
+   */
+  this.aoiListener = function(objRef) {
+    objRef.paint(objRef);
+  }
+  this.model.addListener("aoi",this.aoiListener, this);
+
+  model.addListener("aoi",this.aoiListener, this);
 }
