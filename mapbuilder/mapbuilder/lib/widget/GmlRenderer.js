9a49574b14911cda97a6dd2ad03c53d2a8f99a8e
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2004-08-31 20:05:47 +0000

    Removed paint() stub introduced in last commit

--- mapbuilder/mapbuilder/lib/widget/GmlRenderer.js
+++ mapbuilder/mapbuilder/lib/widget/GmlRenderer.js
@@ -47,8 +47,6 @@ function GmlRenderer(widgetNode, model) {
     //objRef.resultDoc = objRef.coordXsl.transformNodeToObject(objRef.resultDoc);
   }
 
-  //this.paint = function() {}
-
   // Call paint() when the context changes
   this.targetModel.addListener("boundingBox",this.paint, this);
 }
