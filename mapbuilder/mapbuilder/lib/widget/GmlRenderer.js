2ba11e20b2f2dcc2537c162138b9c25e7b24ba35
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2004-08-31 20:42:32 +0000

    Uncomment coordXsl transform

--- mapbuilder/mapbuilder/lib/widget/GmlRenderer.js
+++ mapbuilder/mapbuilder/lib/widget/GmlRenderer.js
@@ -44,7 +44,7 @@ function GmlRenderer(widgetNode, model) {
     objRef.stylesheet.setParameter("bBoxMaxX", bBox[2] );
     objRef.stylesheet.setParameter("bBoxMaxY", bBox[3] );
 
-    //objRef.resultDoc = objRef.coordXsl.transformNodeToObject(objRef.resultDoc);
+    objRef.resultDoc = objRef.coordXsl.transformNodeToObject(objRef.resultDoc);
   }
 
   // Call paint() when the context changes
