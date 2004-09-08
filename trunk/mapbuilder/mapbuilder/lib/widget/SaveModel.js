0980fa06e73a7a35c8cd0067c87cdbf75ec76bba
madair1 <madair1@78ec1247-0520-a048-8ee2-f9631b190d0c>
2004-09-08 20:41:06 +0000

    add save model widget

---
+++ trunk/mapbuilder/mapbuilder/lib/widget/SaveModel.js
@@ -0,0 +1,118 @@
+/*
+
+Author:       Mike Adair mike.adairATccrs.nrcan.gc.ca
+
+License:      GPL as per: http://www.gnu.org/copyleft/gpl.html
+
+
+
+$Id$
+
+*/
+
+
+
+// Ensure this object's dependancies are loaded.
+
+mapbuilder.loadScript(baseDir+"/widget/WidgetBase.js");
+
+
+
+/**
+
+ * Widget to display the AOI box coordinates
+
+ *
+
+ * @constructor
+
+ *
+
+ * @param widgetNode This widget's object node from the configuration document.
+
+ * @param model The model that this widget is a view of.
+
+ */
+
+
+
+function SaveModel(widgetNode, model) {
+
+  var base = new WidgetBase(widgetNode, model);
+
+  for (sProperty in base) { 
+
+    this[sProperty] = base[sProperty]; 
+
+  } 
+
+
+
+  this.stylesheet.setParameter("modelUrl", this.model.url);
+
+  var serializeUrl = widgetNode.selectSingleNode("mb:serializeUrl");
+
+  if (serializeUrl) {
+
+    this.serializeUrl = serializeUrl.firstChild.nodeValue;
+
+    this.stylesheet.setParameter("serializeUrl", this.serializeUrl);
+
+  }
+
+
+
+  /**
+
+   * Refreshes the form onblur handlers when this widget is painted.
+
+   * @param objRef Pointer to this SaveModel object.
+
+   */
+
+  this.saveModel = function() {
+
+    var response = postLoad(this.serializeUrl, this.model.doc);
+
+    response.setProperty("SelectionLanguage", "XPath");
+
+    Sarissa.setXpathNamespaces(response, "xmlns:xlink='http://www.w3.org/1999/xlink'");
+
+    var onlineResource = response.selectSingleNode("//OnlineResource");
+
+    var fileUrl = onlineResource.attributes.getNamedItem("xlink:href").nodeValue;
+
+
+
+    alert(this.model.id+"."+this.id+".modelUrl");
+
+    var modelAnchor = document.getElementById(this.model.id+"."+this.id+".modelUrl");
+
+    modelAnchor.href = fileUrl;
+
+  }
+
+  //this.addListener('paint', this.paintHandler, this);
+
+
+
+  /**
+
+   * Refreshes the form onblur handlers when this widget is painted.
+
+   * @param objRef Pointer to this SaveModel object.
+
+   */
+
+  this.paintHandler = function(objRef) {
+
+    //postLoad('{$serializeUrl}',config.{$modelId}.doc);
+
+  }
+
+  //this.addListener('paint', this.paintHandler, this);
+
+}
+
+
+
