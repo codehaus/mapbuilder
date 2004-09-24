e3d1391f6afd6d034e309e68956eb8e535a72c60
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2004-09-24 20:56:21 +0000

    Commented out functions that are not used (and not relevant?)

--- trunk/mapbuilder/mapbuilder/lib/model/Gml2.js
+++ trunk/mapbuilder/mapbuilder/lib/model/Gml2.js
@@ -30,73 +30,73 @@ function Gml2(modelNode, parent) {
     this[sProperty] = modelBase[sProperty]; 
   }
 
-  // ===============================
-  // Update of GML2 Parameters
-  // ===============================
-
-  /**
-   * Get the BoundingBox.
-   * @return BoundingBox array in form (xmin,ymin,xmax,ymax).
-   */
-  this.getBoundingBox=function() {
-    // Extract BoundingBox from the document
-    var boundingBox = this.doc.selectSingleNode("/ntslookup/gml:boundedBy/gml:Box/gml:coordinates");
-    if ( boundingBox ) {
-      var corners = boundingBox.firstChild.nodeValue.split(" ",2);
-      var bbox = new Array();
-      var minCorner = corners[0].split(",",2);
-      var maxCorner = corners[1].split(",",2);
-      bbox[0]=parseFloat(minCorner[0]);
-      bbox[1]=parseFloat(minCorner[1]);
-      bbox[2]=parseFloat(maxCorner[0]);
-      bbox[3]=parseFloat(maxCorner[1]);
-      return bbox;
-    } else {
-      alert("error getting GML2 bounding box");
-    }
-  }
-
-  /**
-   * Set the BoundingBox and notify intererested widgets that BoundingBox has changed.
-   * @param boundingBox array in form (xmin, ymin, xmax, ymax).
-   */
-  this.setBoundingBox=function(boundingBox) {
-    // Set BoundingBox in the GML
-    //TBD
-    // Call the listeners
-    this.callListeners("boundingBox");
-  }
-
-  /**
-   * Get the Window width.
-   * @return width The width of map window (therefore of map layer images).
-   */
-  this.getWindowWidth=function() {
-    return this.width;
-  }
-
-  /**
-   * Set the Window width.
-   * @param width The width of map window (therefore of map layer images).
-   */
-  this.setWindowWidth=function(width) {
-    this.width = width;
-  }
-
-  /**
-   * Get the Window height.
-   * @return height The height of map window (therefore of map layer images).
-   */
-  this.getWindowHeight=function() {
-    return this.height;
-  }
-
-  /**
-   * Set the Window height.
-   * @param height The height of map window (therefore of map layer images).
-   */
-  this.setWindowHeight=function(height) {
-    this.height = height;
-  }
+//  // ===============================
+//  // Update of GML2 Parameters
+//  // ===============================
+//
+//  /**
+//   * Get the BoundingBox.
+//   * @return BoundingBox array in form (xmin,ymin,xmax,ymax).
+//   */
+//  this.getBoundingBox=function() {
+//    // Extract BoundingBox from the document
+//    var boundingBox = this.doc.selectSingleNode("/ntslookup/gml:boundedBy/gml:Box/gml:coordinates");
+//    if ( boundingBox ) {
+//      var corners = boundingBox.firstChild.nodeValue.split(" ",2);
+//      var bbox = new Array();
+//      var minCorner = corners[0].split(",",2);
+//      var maxCorner = corners[1].split(",",2);
+//      bbox[0]=parseFloat(minCorner[0]);
+//      bbox[1]=parseFloat(minCorner[1]);
+//      bbox[2]=parseFloat(maxCorner[0]);
+//      bbox[3]=parseFloat(maxCorner[1]);
+//      return bbox;
+//    } else {
+//      alert("error getting GML2 bounding box");
+//    }
+//  }
+//
+//  /**
+//   * Set the BoundingBox and notify intererested widgets that BoundingBox has changed.
+//   * @param boundingBox array in form (xmin, ymin, xmax, ymax).
+//   */
+//  this.setBoundingBox=function(boundingBox) {
+//    // Set BoundingBox in the GML
+//    //TBD
+//    // Call the listeners
+//    this.callListeners("boundingBox");
+//  }
+//
+//  /**
+//   * Get the Window width.
+//   * @return width The width of map window (therefore of map layer images).
+//   */
+//  this.getWindowWidth=function() {
+//    return this.width;
+//  }
+//
+//  /**
+//   * Set the Window width.
+//   * @param width The width of map window (therefore of map layer images).
+//   */
+//  this.setWindowWidth=function(width) {
+//    this.width = width;
+//  }
+//
+//  /**
+//   * Get the Window height.
+//   * @return height The height of map window (therefore of map layer images).
+//   */
+//  this.getWindowHeight=function() {
+//    return this.height;
+//  }
+//
+//  /**
+//   * Set the Window height.
+//   * @param height The height of map window (therefore of map layer images).
+//   */
+//  this.setWindowHeight=function(height) {
+//    this.height = height;
+//  }
 }
 
