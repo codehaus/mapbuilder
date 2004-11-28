3dd4b9d70aeb646010a7c67885f336df42dceff4
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2004-11-28 09:44:46 +0000

    Now extend EditButtonBase.js

--- mapbuilder/mapbuilder/lib/tool/AddPoint.js
+++ mapbuilder/mapbuilder/lib/tool/AddPoint.js
@@ -4,33 +4,21 @@ $Id$
 */
 
 // Ensure this object's dependancies are loaded.
-mapbuilder.loadScript(baseDir+"/tool/ButtonBase.js");
+mapbuilder.loadScript(baseDir+"/tool/EditButtonBase.js");
 
 /**
  * When this button is selected, clicks on the MapPane will add a
  * new point to a model.
  * Requires an enclosing GML model.
  * @constructor
- * @base ButtonBase
+ * @base EditButtonBase
  * @author Cameron Shorter cameronATshorter.net
  * @param toolNode      The tool node from the Config XML file.
  * @param parentWidget  The ButtonBar widget.
  */
 function AddPoint(toolNode, parentWidget) {
-  /** Other required tools. */
-  this.dependancies=["MouseClickHandler"];
-
-  // Extend ButtonBase
-  var base = new ButtonBase(this, toolNode, parentWidget);
-
-  /** Empty GML to load when this tool is selected. */
-  this.defaultModelUrl=toolNode.selectSingleNode("mb:defaultModelUrl").firstChild.nodeValue;
-  /** Model to update when a feature is added. */
-  this.targetGmlId=toolNode.selectSingleNode("mb:targetGml").firstChild.nodeValue;
-  this.targetGml=eval("config."+this.targetGmlId);
-
-  /** Reference to GML node to update when a feature is added. */
-  this.featureXpath=toolNode.selectSingleNode("mb:featureXpath").firstChild.nodeValue;
+  // Extend EditButtonBase
+  var base = new EditButtonBase(this, toolNode, parentWidget);
 
   /**
    * Add a point to the enclosing GML model.
@@ -45,31 +33,4 @@ function AddPoint(toolNode, parentWidget) {
       objRef.targetGml.setParam("refresh");
     }
   }
-
-  /**
-   * Load the default GML feature sepecified in the config file.
-   * @param objRef Pointer to this object.
-   * @param selected True when selected.
-   */
-  this.doSelect = function(selected,objRef) {
-    if (objRef.enabled && selected) {
-      // load default GML
-      var httpPayload=new Object();
-      httpPayload.url=objRef.defaultModelUrl;
-      httpPayload.method="get";
-      httpPayload.postData=null;
-      objRef.targetGml.setParam('httpPayload',httpPayload);
-    }
-  }
-
-  /**
-   * Register for mouseup events, called after model loads.
-   * @param objRef Pointer to this object.
-   */
-  this.setMouseListener = function(objRef) {
-    if (objRef.mouseHandler) {
-      objRef.mouseHandler.addListener('mouseup',objRef.doAction,objRef);
-    }
-  }
-  this.parentWidget.targetModel.addListener("loadModel",this.setMouseListener, this);
 }
