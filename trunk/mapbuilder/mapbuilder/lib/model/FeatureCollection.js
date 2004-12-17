de5f2798873230f3ece5ec8225153d49108a03f2
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2004-12-17 09:20:09 +0000

    Initial version moved from Gml2.js and GetFeature.js

---
+++ trunk/mapbuilder/mapbuilder/lib/model/FeatureCollection.js
@@ -0,0 +1,25 @@
+/*
+License: GPL as per: http://www.gnu.org/copyleft/gpl.html
+$Id$
+*/
+
+// Ensure this object's dependancies are loaded.
+mapbuilder.loadScript(baseDir+"/model/ModelBase.js");
+
+/**
+ * Stores a GML Feature or FeatureCollection as defined by the
+ * Open GIS Conortium http://opengis.org.
+ *
+ * @constructor
+ * @base ModelBase
+ * @author Cameron Shorter
+ * @requires Sarissa
+ * @param modelNode The model's XML object node from the configuration document.
+ * @param parent Parent model, if required.
+  */
+function FeatureCollection(modelNode, parent) {
+  // Inherit the ModelBase functions and parameters
+  var modelBase = new ModelBase(this, modelNode, parent);
+  this.namespace = "xmlns:gml='http://www.opengis.net/gml' xmlns:wfs='http://www.opengis.net/wfs'";
+}
+
