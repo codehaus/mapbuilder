88739bd6762952c0f4f31ba42b02902078545918
madair1 <madair1@78ec1247-0520-a048-8ee2-f9631b190d0c>
2005-01-14 04:16:06 +0000

    added check for mapserver WFS bugs

--- mapbuilder/mapbuilder/lib/tool/WebServiceRequest.js
+++ mapbuilder/mapbuilder/lib/tool/WebServiceRequest.js
@@ -24,6 +24,16 @@ function WebServiceRequest(toolNode, model) {
     this.requestName = requestName.firstChild.nodeValue;
   }
 
+  var styleUrl = baseDir+"/tool/xsl/"+this.requestName.replace(/:/,"_")+".xsl";
+  this.stylesheet = new XslProcessor(styleUrl);
+
+  // Set stylesheet parameters for all the child nodes from the config file
+  for (var j=0;j<toolNode.childNodes.length;j++) {
+    if (toolNode.childNodes[j].firstChild && toolNode.childNodes[j].firstChild.nodeValue) {
+      this.stylesheet.setParameter(toolNode.childNodes[j].nodeName,toolNode.childNodes[j].firstChild.nodeValue);
+    }
+  }
+
   /**
    * Listener function which will issue the request
    * @param requestName the name of the web service operation to execute
@@ -38,30 +48,40 @@ function WebServiceRequest(toolNode, model) {
     }
     objRef.targetModel.featureName = featureName;
 
-    var styleUrl = baseDir+"/tool/xsl/"+objRef.requestName.replace(/:/,"_")+".xsl";
-    var requestStylesheet = new XslProcessor(styleUrl);
-
     //confirm inputs
     if (objRef.debug) alert("source:"+feature.xml);
-    //if (objRef.debug) alert("stylesheet:"+requestStylesheet.xslDom.xml);
+    //if (objRef.debug) alert("stylesheet:"+objRef.stylesheet.xslDom.xml);
+
+    if (objRef.targetModel.containerModel) {
+
+      //this block is to get by a Mapserver WFS bug where the tuple separator 
+      //is set to a comma instead of space; and post method doesn't work
+      var ts = " ";
+      var comment = objRef.model.doc.documentElement.childNodes[1];
+      if (comment && comment.nodeType==comment.COMMENT_NODE) {
+        //alert(comment.nodeValue);
+        if (comment.nodeValue.substring("MapServer version 4.4.")) {
+          ts = ",";
+          objRef.targetModel.method="get";
+        }
+      }
+
+      var bBox = objRef.targetModel.containerModel.getBoundingBox();
+      var bboxStr = bBox[0]+","+bBox[1]+ts+bBox[2]+","+bBox[3];
+      objRef.stylesheet.setParameter("bbox", bboxStr );
+      objRef.stylesheet.setParameter("srs", objRef.targetModel.containerModel.getSRS() );
+      objRef.stylesheet.setParameter("width", objRef.targetModel.containerModel.getWindowWidth() );
+      objRef.stylesheet.setParameter("height", objRef.targetModel.containerModel.getWindowHeight() );
+      objRef.stylesheet.setParameter("version", objRef.model.getVersion(feature) );
+    }
 
     //process the doc with the stylesheet
     var httpPayload = new Object();
     httpPayload.method = objRef.targetModel.method;
-    requestStylesheet.setParameter("httpMethod", httpPayload.method );
+    objRef.stylesheet.setParameter("httpMethod", httpPayload.method );
 
-    if (objRef.targetModel.containerModel) {
-      var bBox = objRef.targetModel.containerModel.getBoundingBox();
-      var bboxStr = bBox[0]+","+bBox[1]+","+bBox[2]+","+bBox[3];
-      requestStylesheet.setParameter("bbox", bboxStr );
-      requestStylesheet.setParameter("srs", objRef.targetModel.containerModel.getSRS() );
-      requestStylesheet.setParameter("width", objRef.targetModel.containerModel.getWindowWidth() );
-      requestStylesheet.setParameter("height", objRef.targetModel.containerModel.getWindowHeight() );
-      requestStylesheet.setParameter("version", objRef.model.getVersion(feature) );
-    }
-
-    httpPayload.postData = requestStylesheet.transformNodeToObject(feature);
-    alert("request data:"+httpPayload.postData.xml);
+    httpPayload.postData = objRef.stylesheet.transformNodeToObject(feature);
+    //alert("request data:"+httpPayload.postData.xml);
 
     if (objRef.serverUrl) {
       httpPayload.url = objRef.serverUrl;
