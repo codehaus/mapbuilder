e34d4340f6d659ef3e974d60e4a8117e254927b8
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2005-02-06 09:29:57 +0000

    extracted tool/WebServiceAction out of widget/FeatureList.js

--- trunk/mapbuilder/mapbuilder/lib/widget/FeatureList.js
+++ trunk/mapbuilder/mapbuilder/lib/widget/FeatureList.js
@@ -5,58 +5,32 @@ $Id$
 
 // Ensure this object's dependancies are loaded.
 mapbuilder.loadScript(baseDir+"/widget/WidgetBase.js");
+mapbuilder.loadScript(baseDir+"/tool/WebServiceAction.js");
 
 /**
  * Functions to render and update a FeatureList from GML.
  * @constructor
  * @base WidgetBase
- * @author Cameron Shorter cameronATshorter.net
+ * @requires WebServiceAction
+ * @author Cameron Shorter
  * @param widgetNode  The widget's XML object node from the configuration document.
  * @param model       The model object that this widget belongs to.
  */
 function FeatureList(widgetNode, model) {
   var base = new WidgetBase(this, widgetNode, model);
 
-  /** Url of WFS */
-  this.webServiceUrl=widgetNode.selectSingleNode("mb:webServiceUrl").firstChild.nodeValue;
-
-  /** Xsl to convert Feature into a WFS Transaction Insert. */
-  this.insertXsl=new XslProcessor(baseDir+"/tool/xsl/wfs_Insert.xsl");
+  /** Tool which processes form key presses. */
+  this.webServiceAction=new WebServiceAction();
 
   /**
-   * Process a button press.
+   * Call WebServiceAction.processAction() which processes a button press.
+   * Note, when EditButton is selected, EditButton set params in the model
+   * which are required by WebServiceAction.
    * @param objRef Reference to this object.
    * @param button Button name.
    */
   this.processButton=function(objRef,button){
-    httpPayload=new Object();
-    switch(button){
-      case "Reset":
-        if(objRef.model.url){
-          httpPayload.url=objRef.model.url;
-          httpPayload.method="get";
-          httpPayload.postData=null;
-          objRef.model.newRequest(objRef.model,httpPayload);
-          break;
-        }
-      case "Insert Feature":
-        s=objRef.insertXsl.transformNodeToObject(objRef.model.doc);
-        httpPayload.postData=s;
-        httpPayload.url=objRef.webServiceUrl;
-        httpPayload.method="post";
-        objRef.targetModel.newRequest(objRef.targetModel,httpPayload);
-        sucess=objRef.targetModel.doc.selectSingleNode("//wfs:TransactionResult/wfs:Status/wfs:SUCCESS");
-        if (sucess){
-          // Remove FeatureList of feature entry was successful.
-          httpPayload.url=null;
-          objRef.model.newRequest(objRef.model,httpPayload);
-        }
-        break;
-      case "Update Feature":
-        alert("FeatureList: Update Feature not implemented");
-        break;
-      default:
-        alert("FeatureList: Unknown button: "+button);
-    }
+    objRef.webServiceAction.processAction(
+      objRef.webServiceAction,objRef.model,button);
   }
 }
