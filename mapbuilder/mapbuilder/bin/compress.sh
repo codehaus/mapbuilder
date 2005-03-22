887cc8ffecc6c455ae5663c45710416acc693e78
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2005-03-22 19:27:14 +0000

    Expect source files to be in the build directory

--- mapbuilder/mapbuilder/bin/compress.sh
+++ mapbuilder/mapbuilder/bin/compress.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 # Compress all javascript files using jsjam.pl
 # Use xsltproc to compress XSL files
+# This script expects files to be located in ../build/lib/*
 # 
 # $Id$
 
 # Set variables
 mapbuilderDir=`dirname $0`/..;
+targetDir=${mapbuilderDir}/build
 tmp="jsjam.tmp";
 
 # Compress javascript files
-for file in `find /home/cameron/work/x/lib -name "*.js"` ; do
+for file in `find ${targetDir}/lib -name "*.js"` ; do
   ${mapbuilderDir}/bin/jsjam.pl ${file} -i -g -n > ${tmp};
   mv ${tmp} ${file};
 done;
 
 # Compress xsl files
-for file in `find /home/cameron/work/x/lib -name "*.xsl"` ; do
+for file in `find ${targetDir}/lib -name "*.xsl"` ; do
   xsltproc ${mapbuilderDir}/bin/compressXsl.xsl ${file} > ${tmp};
   mv ${tmp} ${file};
 done;
