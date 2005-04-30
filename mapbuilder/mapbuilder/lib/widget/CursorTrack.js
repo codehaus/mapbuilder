f0c2028a2d999d88f98bff4eb73ff44e453903a6
madair1 <madair1@78ec1247-0520-a048-8ee2-f9631b190d0c>
2005-04-30 17:22:35 +0000

    add optional output in map XY

--- mapbuilder/mapbuilder/lib/widget/CursorTrack.js
+++ mapbuilder/mapbuilder/lib/widget/CursorTrack.js
@@ -10,8 +10,7 @@ mapbuilder.loadScript(baseDir+"/widget/WidgetBase.js");
 mapbuilder.loadScript(baseDir+"/model/Proj.js");
 
 /**
- * Widget to display the mouse coordinates when it is over a sibling mappane
- * widget.
+ * Widget to display the mouse coordinates when it is over a MapContainer widget.
  *
  * @constructor
  * @base WidgetBase
@@ -22,12 +21,17 @@ mapbuilder.loadScript(baseDir+"/model/Proj.js");
 function CursorTrack(widgetNode, model) {
   var base = new WidgetBase(this, widgetNode, model);
 
+  //by default, display coords in latlon; if false show map XY
+  this.showXY = false;
+  var showXYNode = widgetNode.selectSingleNode("mb:showXY");
+  if (showXYNode) this.showXY = (showXYNode.firstChild.nodeValue=="false")?false:true;
+
   /**
-   * Start cursor tracking when the mouse is over the mappane.
+   * Start cursor tracking when the mouse is over the MapContainer.
    * set an interval to output the coords so that it doesn't execute on every
    * move event.  The setInterval method in IE doesn't allow passing of an
-   * argument to the function called so set a global reference to glass pane
-   * here;  mouse can only be over one glass pane at time so this should be safe.
+   * argument to the function called so set a global reference to MapContainer
+   * here;  mouse can only be over one MapContainer at time so this should be safe.
    * @param objRef Pointer to this CurorTrack object.
    * @param targetNode The node for the enclosing HTML tag for this widget.
    */
@@ -40,7 +44,7 @@ function CursorTrack(widgetNode, model) {
   }
 
   /**
-   * Stop cursor tracking when the mouse is over the mappane.
+   * Stop cursor tracking when the mouse leaves the MapContainer.
    * @param objRef Pointer to this CurorTrack object.
    * @param targetNode The node for the enclosing HTML tag for this widget.
    */
@@ -51,18 +55,22 @@ function CursorTrack(widgetNode, model) {
     objRef.coordForm.latitude.value = "";
   }
 
-  //initialize dynamic properties
-  this.init = function(toolRef) {
+  /**
+   * Add mouse event listeners to the MapContainer object sepecified by the
+   * mouseHandler property in config.
+   * @param objRef Pointer to this CurorTrack object.
+   */
+  this.init = function(objRef) {
     //associate the cursor track with a mappane widget
     var mouseHandler = widgetNode.selectSingleNode("mb:mouseHandler");
     if (mouseHandler) {
-      toolRef.mouseHandler = eval("config.objects."+mouseHandler.firstChild.nodeValue);
-      toolRef.mouseHandler.addListener('mouseover', toolRef.mouseOverHandler, toolRef);
-      toolRef.mouseHandler.addListener('mouseout', toolRef.mouseOutHandler, toolRef);
+      objRef.mouseHandler = eval("config.objects."+mouseHandler.firstChild.nodeValue);
+      objRef.mouseHandler.addListener('mouseover', objRef.mouseOverHandler, objRef);
+      objRef.mouseHandler.addListener('mouseout', objRef.mouseOutHandler, objRef);
     } else {
       alert('CursorTrack requires a mouseHandler property');
     }
-    toolRef.proj = new Proj( toolRef.model.getSRS() );
+    if (!objRef.showXY) objRef.proj = new Proj( objRef.model.getSRS() );
   }
   this.model.addListener("loadModel", this.init, this );
 
@@ -73,13 +81,16 @@ function CursorTrack(widgetNode, model) {
 
 }
 
-/** Update the lat/long coordinates in coordForm. */
+/**
+ * A method which actually outputs the coordinates to the form.  This is a
+ * global method because it is called by a Javascript setInterval timer.
+ */
 function ReportCoords() {
   var objRef = window.cursorTrackObject;
   if (objRef.mouseOver) {
     var evxy = objRef.model.extent.getXY( window.cursorTrackNode.evpl );
-    var evll = objRef.proj.Inverse( evxy );
-    objRef.coordForm.longitude.value = Math.round(evll[0]*100)/100;
-    objRef.coordForm.latitude.value = Math.round(evll[1]*100)/100;
+    if (!objRef.showXY) evxy = objRef.proj.Inverse( evxy );   //convert to lat/long
+    objRef.coordForm.longitude.value = Math.round(evxy[0]*100)/100;
+    objRef.coordForm.latitude.value = Math.round(evxy[1]*100)/100;
   }
 }
