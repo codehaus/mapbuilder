abc9c1b71c73c74051b7458f3e2109ddaed723f4
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2005-11-01 09:38:26 +0000

    Added cursor selection to doSelect

--- trunk/mapbuilder/mapbuilder/lib/widget/EditButtonBase.js
+++ trunk/mapbuilder/mapbuilder/lib/widget/EditButtonBase.js
@@ -38,6 +38,22 @@ function EditButtonBase(widgetNode, model) {
    * @param selected True when selected.
    */
   this.doSelect = function(selected,objRef) {
+    // Set cursor
+    if( selected == true ) {
+      var a = document.getElementById("mainMapContainer");
+      if( a != null ) {
+        // default tool cursor
+        a.style.cursor = this.cursor;
+            
+        // Check for user override
+        var cursorNode =  objRef.widgetNode.selectSingleNode("mb:cursor");
+        if( cursorNode != null ) {
+          var cursor = cursorNode.firstChild.nodeValue;
+          a.style.cursor = cursor;
+        }
+      }  
+    }
+    // Load default feature.
     if (objRef.enabled && selected && objRef.targetModel.url!=objRef.defaultModelUrl){
       a=new Array("transactionResponseModel","webServiceUrl","featureXpath","defaultModelUrl","targetContext");
       for (i in a){
