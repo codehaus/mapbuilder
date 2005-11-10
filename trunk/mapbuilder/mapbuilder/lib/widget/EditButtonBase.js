bcd6168ec3b6570942b6e2227f4246d10e501683
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2005-11-10 10:51:43 +0000

    Bug1116660 Remove the TransactionStatus widget when EditFeatureis selected

--- trunk/mapbuilder/mapbuilder/lib/widget/EditButtonBase.js
+++ trunk/mapbuilder/mapbuilder/lib/widget/EditButtonBase.js
@@ -22,6 +22,9 @@ function EditButtonBase(widgetNode, model) {
   // cursor can be changed by specifying a new cursor in config file
   this.cursor = "crosshair"; 
 
+  this.trm=widgetNode.selectSingleNode("mb:transactionResponseModel");
+  if(this.trm)this.trm=this.trm.firstChild.nodeValue;
+
   /** Empty GML to load when this tool is selected. */
   this.defaultModelUrl=widgetNode.selectSingleNode("mb:defaultModelUrl").firstChild.nodeValue;
 
@@ -32,29 +35,37 @@ function EditButtonBase(widgetNode, model) {
    * If tool is selected and the Edit Tool has changed (eg, changed from
    * LineEdit to PointEdit) then load new default feature.
    * This function is called when a tool is selected or deselected.
-   * The following parameters are copied from this button's config node into
-   * the target model:<br/>
-   * "transactionResponseModel", "webServiceUrl", "featureXpath",
-   * "defaultModelUrl", "targetContext".
-   * These values will be used by WebServiceAction when processing a
-   * transaction.
    * @param objRef Pointer to this object.
    * @param selected True when selected.
    */
   this.doSelect = function(selected,objRef) {
-  
+    // Model that will be populated with the WFS response.
+    if (objRef.trm && !objRef.transactionResponseModel){
+      objRef.transactionResponseModel=eval("config.objects."+objRef.trm);
+    }
     // Load default feature.
     if (objRef.enabled && selected && objRef.targetModel.url!=objRef.defaultModelUrl){
-      objRef.targetModel.url=objRef.defaultModelUrl;
-      // load default GML
-      var httpPayload=new Object();
-      httpPayload.url=objRef.defaultModelUrl;
-      httpPayload.method="get";
-      httpPayload.postData=null;
-      objRef.targetModel.newRequest(objRef.targetModel,httpPayload);
+      objRef.loadDefaultModel(objRef);
+    }
+    // Remove the transactionResponseModel (and result of last transaction)
+    // when a transaction button is deselected
+    if(!selected && objRef.transactionResponseModel){
+      objRef.transactionResponseModel.setModel(objRef.transactionResponseModel,null);
     }
   }
 
+  /**
+   * Load the defaultModel into the targetModel.
+   */
+  this.loadDefaultModel=function(objRef){
+    objRef.targetModel.url=objRef.defaultModelUrl;
+    // load default GML
+    var httpPayload=new Object();
+    httpPayload.url=objRef.defaultModelUrl;
+    httpPayload.method="get";
+    httpPayload.postData=null;
+    objRef.targetModel.newRequest(objRef.targetModel,httpPayload);
+   }
   /**
    * Register for mouseup events, called after model loads.
    * @param objRef Pointer to this object.
