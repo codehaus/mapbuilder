a103ca7a7a6d6b5ad50201dff7494cd2fab2e521
rabla <rabla@78ec1247-0520-a048-8ee2-f9631b190d0c>
2006-01-20 13:15:37 +0000

    register loadModel even also on containerModel

--- trunk/mapbuilder/mapbuilder/lib/model/FeatureCollection.js
+++ trunk/mapbuilder/mapbuilder/lib/model/FeatureCollection.js
@@ -29,7 +29,7 @@ function FeatureCollection(modelNode, parent) {
    * @param objRef Pointer to this object.
    */
   this.convertCoords = function(objRef) {
-    if( objeRef.doc && objRef.containerModel.doc ) {
+    if( objRef.doc && objRef.containerModel.doc ) {
 	  var coordNodes = objRef.doc.selectNodes("//gml:coordinates");
 	  if (coordNodes.length>0 && objRef.containerModel) {
 	    var srsNode = coordNodes[0].selectSingleNode("ancestor-or-self::*/@srsName");
@@ -56,6 +56,7 @@ function FeatureCollection(modelNode, parent) {
     }
   }
   this.addFirstListener("loadModel",this.convertCoords,this);
+  this.containerModel.addListener("loadModel",this.convertCoords,this);
 
   /**
    * Change a feature's visibility.
