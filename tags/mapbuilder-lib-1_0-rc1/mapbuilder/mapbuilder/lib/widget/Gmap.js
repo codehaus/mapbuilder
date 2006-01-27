68930ac7dcec83a1b3b876e5a5a4c39f2bc11d1b
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2006-01-27 12:05:55 +0000

    Got Gmap Zooming sort of working but extent is still out a bit

--- tags/mapbuilder-lib-1_0-rc1/mapbuilder/mapbuilder/lib/widget/Gmap.js
+++ tags/mapbuilder-lib-1_0-rc1/mapbuilder/mapbuilder/lib/widget/Gmap.js
@@ -2,11 +2,12 @@
 Author:       Cameron Shorter cameronATshorter.net
 License:      LGPL as per: http://www.gnu.org/copyleft/lesser.html
 
-$Id: GmlRendererWZ.js 1671 2005-09-20 02:37:54Z madair1 $
+$Id: $
 */
 
 // Ensure this object's dependancies are loaded.
 mapbuilder.loadScript(baseDir+"/widget/MapContainerBase.js");
+mapbuilder.loadScript(baseDir+"/tool/GmapTools.js");
 
 /**
  * Create a map Layer using Google Maps.
@@ -25,19 +26,17 @@ function Gmap(widgetNode, model) {
     if (objRef.model.doc && objRef.node) {
 
       var outputNode = document.getElementById( objRef.outputNodeId );
-      var tempNode = document.createElement("DIV");
-
-      tempNode.style.position="absolute";
-      tempNode.style.top=0;
-      tempNode.style.left=0;
-      tempNode.style.width=objRef.model.getWindowWidth();
-      tempNode.style.height=objRef.model.getWindowHeight();
-      tempNode.style.zindex=300;
-      tempNode.setAttribute("id", objRef.outputNodeId);
-      //look for this widgets output and replace if found, otherwise append it
-      if (outputNode) {
-        objRef.node.replaceChild(tempNode,outputNode);
-      } else {
+      //look for this widgets output and replace if found, otherwise
+      //append it
+      if (!outputNode) {
+        tempNode = document.createElement("DIV");
+        tempNode.style.position="absolute";
+        tempNode.style.top=0;
+        tempNode.style.left=0;
+        tempNode.style.width=objRef.model.getWindowWidth();
+        tempNode.style.height=objRef.model.getWindowHeight();
+        tempNode.style.zindex=300;
+        tempNode.setAttribute("id", objRef.outputNodeId);
         objRef.node.appendChild(tempNode);
       }
     }
@@ -45,10 +44,12 @@ function Gmap(widgetNode, model) {
     //TBD This should be moved to an initialisation function
     if(!objRef.gmap){
       objRef.gmap=new GMap(tempNode);
+      objRef.gmapTools=new GmapTools();
+      objRef.model.setParam("gmap",objRef.gmap);
     }
 
-    //TBD Needs to be replaced with real coordinates and zoom level
-    objRef.gmap.centerAndZoom(new GPoint(-122.1419, 37.4419),4);
+    objRef.gmapTools.centerAndZoom(objRef.model);
+    //objRef.gmap.centerAndZoom(new GPoint(-122.1419, 37.4419),4);
 
   }
   this.model.addListener("refresh",this.paint, this);
