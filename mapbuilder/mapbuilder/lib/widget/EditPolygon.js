b0bba6c5ca35cde2d17caccd70f32429e3ca10a1
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2006-02-05 00:13:37 +0000

    Fixes for Polygon submitted on behalf of Simon Flannery

--- mapbuilder/mapbuilder/lib/widget/EditPolygon.js
+++ mapbuilder/mapbuilder/lib/widget/EditPolygon.js
@@ -11,33 +11,56 @@ mapbuilder.loadScript(baseDir+"/widget/EditButtonBase.js");
  * new point to a polygon.
  * @constructor
  * @base EditButtonBase
- * @author Cameron Shorter cameronATshorter.net
+ * @author Simon Flannery simonDOTflanneryATbigpondDOTcom
+ * @sponser VPAC
  * @param widgetNode The node from the Config XML file.
- * @param model  The ButtonBar widget.
+ * @param model The ButtonBar widget.
  */
 function EditPolygon(widgetNode, model) {
   // Extend EditButtonBase
   EditButtonBase.apply(this, new Array(widgetNode, model));
 
   /**
-   * Append a point to a polygon.
+   * If the number of exsisting points is less than 2, append the new point to the polygon.
+   * If the number of exsisting points is equal to 2, append the new point AND the first point again.
+   * If the number of exsisting points is greater than 2, remove the last point (the first point), and append the new point AND the first point again.
    * @param objRef      Pointer to this object.
    * @param targetNode  The node for the enclosing HTML tag for this widget.
    */
-  this.doAction = function(objRef,targetNode) {
+  this.doAction = function(objRef, targetNode) {
     if (objRef.enabled) {
-      point=objRef.mouseHandler.model.extent.getXY(targetNode.evpl);
-      old=objRef.targetModel.getXpathValue(
-        objRef.targetModel,
-        objRef.featureXpath);
-      if(!old){old=""}
-      sucess=objRef.targetModel.setXpathValue(
-        objRef.targetModel,
-        objRef.featureXpath,
-        old+" "+point[0]+","+point[1]);
-      if(!sucess){
-        alert("EditPolygon: invalid featureXpath in config: "+objRef.featureXpath);
+      var point = objRef.mouseHandler.model.extent.getXY(targetNode.evpl);
+      var old   = objRef.targetModel.getXpathValue(objRef.targetModel, objRef.featureXpath);
+      var thePolygon = "";
+
+      if (!old) {
+         thePolygon = point[0] + "," + point[1];
+      }
+      else {
+         var collect = old.split(" "); /* The string is already delimited by white space. */
+
+         if (collect.length < 2) { /* Enforce the polygon rules. */
+            thePolygon = old + " " + point[0] + "," + point[1];
+         }
+         else if (collect.length == 2) {
+            thePolygon = old + " " + point[0] + "," + point[1] + " " + collect[0];
+         }
+         else if (collect.length > 2) {
+            for (var i = 0; i < collect.length - 1; ++i) {
+
+               thePolygon = thePolygon + collect[i] + " ";
+            }
+
+            thePolygon = thePolygon + point[0] + "," + point[1] + " " + collect[0];
+         }
+      }
+
+      sucess = objRef.targetModel.setXpathValue(objRef.targetModel, objRef.featureXpath, thePolygon);
+
+      if (!sucess) {
+        alert("EditPolygon: invalid featureXpath in config: " + objRef.featureXpath);
       }
     }
   }
 }
+
