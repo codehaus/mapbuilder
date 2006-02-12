032ab1975a689bc9cc3d97c03e3d45e2c405f7e1
mattdiez <mattdiez@78ec1247-0520-a048-8ee2-f9631b190d0c>
2006-02-12 23:04:33 +0000

    Necessary Remove.

--- tags/mapbuilder-lib-1_0-rc2/mapbuilder/fitnesse/ruby/test/fixtures/fail_fixture.rb
+++
@@ -1,9 +0,0 @@
-require 'fit/fixture'
-
-module Fixtures
-  class FailFixture < Fit::Fixture
-    def do_table(table)
-      wrong(table.parts.parts)
-    end
-  end
-end
--- tags/mapbuilder-lib-1_0-rc2/mapbuilder/fitnesse/run.bat
+++
@@ -1,3 +0,0 @@
-
-java -cp fitnesse.jar fitnesse.FitNesse -p 8088 -e 0 %1 %2 %3 %4 %5
-pause
