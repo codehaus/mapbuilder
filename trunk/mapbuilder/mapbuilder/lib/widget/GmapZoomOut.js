9d680eb58287a5214bb972de2c462fb11eb8f514
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2006-03-20 10:46:21 +0000

    Add Gmap Zoom/Pan tools

--- trunk/mapbuilder/mapbuilder/lib/widget/GmapZoomOut.js
+++ trunk/mapbuilder/mapbuilder/lib/widget/GmapZoomOut.js
@@ -1,6 +1,6 @@
 /*
 License: LGPL as per: http://www.gnu.org/copyleft/lesser.html
-$Id: ZoomIn.js 1671 2005-09-20 02:37:54Z madair1 $
+$Id: ZoomOut.js 1671 2005-09-20 02:37:54Z madair1 $
 */
 
 // Ensure this object's dependancies are loaded.
@@ -8,38 +8,30 @@ mapbuilder.loadScript(baseDir+"/widget/ButtonBase.js");
 mapbuilder.loadScript(baseDir+"/tool/GoogleMapTools.js");
 
 /**
- * When this button is selected, clicks on the MapPane trigger a zoomIn to the 
- * currently set AOI.
+ * When this button is selected, click on the map to zoom out centered at the click.
  * @constructor
  * @base ButtonBase
  * @author Mike Adair mike.adairATccrs.nrcan.gc.ca
  * @param widgetNode The widget node from the Config XML file.
  * @param model  The model for this widget
  */
-function GmapZoomIn(widgetNode, model) {
+function GmapZoomOut(widgetNode, model) {
   // Extend ButtonBase
   ButtonBase.apply(this, new Array(widgetNode, model));
 
   /**
-   * Calls the model's ceter at method to zoom in.  If the AOI is a single point,
-   * it zooms in by the zoomBy factor.
-   * @param objRef      Pointer to this object.
+   * Calls the centerAt method of the context doc to zoom out, recentering at
+   * the mouse event coordinates.
+   * @param objRef      Pointer to this AoiMouseHandler object.
    * @param targetNode  The node for the enclosing HTML tag for this widget.
    */
   this.doAction = function(objRef,targetNode) {
-    if (objRef.enabled) {
-      var bbox = objRef.targetModel.getParam("aoi");
-      if ( bbox!=null) {
-        var extent = objRef.targetModel.extent;
-        var ul = bbox[0];
-        var lr = bbox[1];
-        if ( ( ul[0]==lr[0] ) && ( ul[1]==lr[1] ) ) {
-          objRef.googleMapTools.zoomTo(objRef.targetModel,ul,-1);
-        } else {
-          objRef.googleMapTools.setGmapExtent(objRef.targetModel,new Array(lr[0],ul[1],ul[0],lr[1]));
-        }
-      }
-    }
+    if (!objRef.enabled) return;
+    var bbox = objRef.targetModel.getParam("aoi");
+    objRef.googleMapTools.zoomTo(
+      objRef.targetModel,
+      new Array((bbox[0][0]+bbox[1][0])/2,(bbox[1][0]+bbox[1][1])/2),
+      1);
   }
 
   /**
@@ -58,4 +50,3 @@ function GmapZoomIn(widgetNode, model) {
   this.model.addListener( "loadModel", this.setMouseListener, this );
 
 }
-
