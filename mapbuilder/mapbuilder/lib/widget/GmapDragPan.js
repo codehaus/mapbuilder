2d17b704b30f9430ae491dcb8c882338efa48958
camerons <camerons@78ec1247-0520-a048-8ee2-f9631b190d0c>
2006-03-20 10:46:21 +0000

    Add Gmap Zoom/Pan tools

---
+++ mapbuilder/mapbuilder/lib/widget/GmapDragPan.js
@@ -0,0 +1,57 @@
+/*
+License: LGPL as per: http://www.gnu.org/copyleft/lesser.html
+$Id: DragPan.js 1671 2005-09-20 02:37:54Z madair1 $
+*/
+
+// Ensure this object's dependancies are loaded.
+mapbuilder.loadScript(baseDir+"/widget/ButtonBase.js");
+mapbuilder.loadScript(baseDir+"/tool/GoogleMapTools.js");
+
+
+/**
+ * When this button is selected, click and drag on the MapPane to recenter the map.
+ * @constructor
+ * @base ButtonBase
+ * @author Mike Adair mike.adairATccrs.nrcan.gc.ca
+ * @param widgetNode  The widget node from the Config XML file.
+ * @param model  The parent model for this widget
+ */
+function GmapDragPan(widgetNode, model) {
+
+  // Extend ButtonBase
+  ButtonBase.apply(this, new Array(widgetNode, model));
+
+  // override default cursor by user
+  // cursor can be changed by spefying a new cursor in config file
+  this.cursor = "move";	
+
+  /**
+   * Calls the centerAt method of the context doc extent to recenter to its AOI
+   * @param objRef      Pointer to this DragPan object.
+   * @param targetNode  The node for the enclosing HTML tag for this widget.
+   */
+  this.doAction = function(objRef,targetNode) {
+    if (!objRef.enabled) return;
+    var bbox = objRef.targetModel.getParam("aoi");
+    objRef.googleMapTools.zoomTo(
+      objRef.targetModel,
+      new Array((bbox[0][0]+bbox[1][0])/2,(bbox[1][0]+bbox[1][1])/2),
+      0);
+  }
+
+  /**
+   * Register for mouseUp events.
+   * @param objRef      Pointer to this DragPan object.
+   */
+  this.setMouseListener = function(objRef) {
+    if (objRef.mouseHandler) {
+      objRef.mouseHandler.model.addListener('mouseup',objRef.doAction,objRef);
+    }
+    // Create a googleMapTools instance
+    if(!objRef.googleMapTools){
+      objRef.googleMapTools=new GoogleMapTools();
+    }
+  }
+  this.model.addListener( "refresh", this.setMouseListener, this );
+
+}
