ea936e8eddad9d9ef235cc8b2824640fa514322f
steven <steven@78ec1247-0520-a048-8ee2-f9631b190d0c>
2006-10-10 12:02:12 +0000

    A widget to select a single feature with a mouse click, it's rather rough now

---
+++ sandbox/steven/FeatureEditing/lib/widget/GetFeature.js
@@ -0,0 +1,51 @@
+/*
+License: LGPL as per: http://www.gnu.org/copyleft/lesser.html
+$Id: ZoomOut.js 2133 2006-06-22 15:28:52Z steven $
+*/
+
+// Ensure this object's dependancies are loaded.
+mapbuilder.loadScript(baseDir+"/widget/ButtonBase.js");
+
+/**
+ * When this button is selected, click on the map to zoom out centered at the click.
+ * @constructor
+ * @base ButtonBase
+ * @author Mike Adair mike.adairATccrs.nrcan.gc.ca
+ * @param widgetNode The widget node from the Config XML file.
+ * @param model  The model for this widget
+ */
+function GetFeature(widgetNode, model) {
+  // Extend ButtonBase
+  ButtonBase.apply(this, new Array(widgetNode, model));
+
+  /**
+   * Calls the centerAt method of the context doc to zoom out, recentering at 
+   * the mouse event coordinates.
+   * TBD: set the zoomFactor property as a button property in conifg
+   * @param objRef      Pointer to this AoiMouseHandler object.
+   * @param targetNode  The node for the enclosing HTML tag for this widget.
+   */
+  this.doAction = function(objRef,targetNode) {
+    if (objRef.enabled) {
+     var point=objRef.mouseHandler.model.extent.getXY(targetNode.evpl);
+      var x = point[0];
+      var y = point[1];
+      objRef.targetModel.setParam("aoi",new Array(new Array(x-10,y-10),new Array(x+10,y+10)));
+      config.objects.webServiceForm.submitForm();
+      
+      
+    }
+  }
+
+  /**
+   * Register for mouseUp events.
+   * @param objRef  Pointer to this object.
+   */
+  this.setMouseListener = function(objRef) {
+    if (objRef.mouseHandler) {
+      objRef.mouseHandler.model.addListener('mouseup',objRef.doAction,objRef);
+    }
+  }
+  this.model.addListener( "loadModel", this.setMouseListener, this );
+
+}
