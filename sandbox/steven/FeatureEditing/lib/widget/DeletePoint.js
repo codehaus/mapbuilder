324e914c5d5bb73d8484ad13a92af8797bb62500
steven <steven@78ec1247-0520-a048-8ee2-f9631b190d0c>
2006-11-03 12:11:38 +0000

    Simple widget to enable point deletion

---
+++ sandbox/steven/FeatureEditing/lib/widget/DeletePoint.js
@@ -0,0 +1,39 @@
+/*
+License: LGPL as per: http://www.gnu.org/copyleft/lesser.html
+$Id: ZoomOut.js 2133 2006-06-22 15:28:52Z steven $
+*/
+
+// Ensure this object's dependancies are loaded.
+mapbuilder.loadScript(baseDir+"/widget/ButtonBase.js");
+
+/**
+ * When this button is selected, click on the map to zoom out centered at the click.
+ * @constructor
+ * @base ButtonBase
+ * @author Mike Adair mike.adairATccrs.nrcan.gc.ca
+ * @param widgetNode The widget node from the Config XML file.
+ * @param model  The model for this widget
+ */
+function DeletePoint(widgetNode, model) {
+  // Extend ButtonBase
+  ButtonBase.apply(this, new Array(widgetNode, model));
+ this.cursor = "select";	
+  /**
+   * Calls the centerAt method of the context doc to zoom out, recentering at 
+   * the mouse event coordinates.
+   * TBD: set the zoomFactor property as a button property in conifg
+   * @param objRef      Pointer to this AoiMouseHandler object.
+   * @param targetNode  The node for the enclosing HTML tag for this widget.
+   */
+  this.doSelect = function(selected,objRef) {
+    if (selected){
+
+		this.targetModel.setParam("delpoint",true);
+    }
+    else {
+    		this.targetModel.setParam("delpoint",false);
+    }
+    
+  }
+  
+}
