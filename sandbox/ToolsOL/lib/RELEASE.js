ee0216fd9f7dea2450eb09431e370438e55d92d4
oterral <oterral@78ec1247-0520-a048-8ee2-f9631b190d0c>
2007-02-01 10:49:07 +0000

    delete folder demo and lib

--- sandbox/ToolsOL/lib/RELEASE.js
+++
@@ -1,6 +0,0 @@
-// This variable is set when script compression is used
-// It will disable dynamic script loading within MapBuilder
-// instead use the compressed file buil tfor that application
-
-var MapBuilder_Release = true
-//var baseDir = "../../lib"
