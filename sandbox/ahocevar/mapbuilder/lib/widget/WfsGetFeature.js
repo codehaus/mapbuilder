a7b38c14d4f61f8a6110a6fd47b9fa383c0e68d6
ahocevar <ahocevar@78ec1247-0520-a048-8ee2-f9631b190d0c>
2007-04-02 23:45:09 +0000

    New WfsGetFeature widget to work with MapPaneOL

--- sandbox/ahocevar/mapbuilder/lib/widget/WfsGetFeature.js
+++ sandbox/ahocevar/mapbuilder/lib/widget/WfsGetFeature.js
@@ -6,6 +6,7 @@ $Id$
 
 // Ensure this object's dependancies are loaded.
 mapbuilder.loadScript(baseDir+"/tool/ButtonBase.js");
+mapbuilder.loadScript(baseDir+"/util/openlayers/OpenLayers.js");
 /**
  * Builds then sends a WFS GetFeature GET request based on the WMC
  * coordinates and click point.
@@ -21,56 +22,66 @@ function WfsGetFeature(widgetNode, model) {
   this.tolerance= widgetNode.selectSingleNode('mb:tolerance').firstChild.nodeValue;
   this.typeName= widgetNode.selectSingleNode('mb:typeName').firstChild.nodeValue;
   this.webServiceUrl= widgetNode.selectSingleNode('mb:webServiceUrl').firstChild.nodeValue;
+  this.webServiceUrl += this.webServiceUrl.indexOf("?") > -1 ? '&' : '?';
   this.httpPayload=new Object();
   this.httpPayload.method="get";
   this.httpPayload.postData=null;
   this.trm=widgetNode.selectSingleNode("mb:transactionResponseModel").firstChild.nodeValue;
+  this.maxFeatures = widgetNode.selectSingleNode('mb:maxFeatures');
+  this.maxFeatures = this.maxFeatures ? this.maxFeatures.firstChild.nodeValue : 1;
 
   // override default cursor by user
   // cursor can be changed by spefying a new cursor in config file
+  //TBD this does nothing with MapPaneOL yet
   this.cursor = "pointer"; 
 
-  /**
-   * Open window with query info.
-   * This function is called when user clicks map with Query tool.
-   * @param objRef      Pointer to this GetFeatureInfo object.
-   * @param targetNode  The node for the enclosing HTML tag for this widget.
-   */
-  this.doAction = function(objRef,targetNode) {
-    if (objRef.enabled) {
-      extent=objRef.targetModel.extent;
-      point=extent.getXY(targetNode.evpl);
-      xPixel=extent.res[0]*objRef.tolerance;
-      yPixel=extent.res[1]*objRef.tolerance;
-      bbox=(point[0]-xPixel)+","+(point[1]-yPixel)+","+(point[0]+xPixel)+","+(point[1]+yPixel);
-      if (objRef.webServiceUrl.indexOf("?") > -1){
-        objRef.httpPayload.url=objRef.webServiceUrl+"&request=GetFeature&typeName="+objRef.typeName+"&bbox="+bbox;
-      }else{
-        objRef.httpPayload.url=objRef.webServiceUrl+"?request=GetFeature&typeName="+objRef.typeName+"&bbox="+bbox;
-      }
+  this.createControl = function(objRef) {
+    var Control = OpenLayers.Class.create();
+    Control.prototype = OpenLayers.Class.inherit( OpenLayers.Control, {
+      CLASS_NAME: 'mbControl.WfsGetFeature',
+      type: OpenLayers.Control.TYPE_TOOL, // constant from OpenLayers.Control
+      
+      // properties from this widget
+      tolerance: this.tolerance/2,
+      typeName: this.typeName,
+      maxFeatures: this.maxFeatures,
+      webServiceUrl: this.webServiceUrl,
+      httpPayload: this.httpPayload,
+      trm: window.config.objects[objRef.trm],
+      
+      draw: function() {
+        this.handler = new OpenLayers.Handler.Box( this,
+          {done: this.selectBox}, {keyMask: this.keyMask} );
+      },
 
-      // Model that will be populated with the WFS response.
-      if (!objRef.transactionResponseModel){
-        objRef.transactionResponseModel=window.config.objects[objRef.trm];
-        //objRef.transactionResponseModel.addListener("loadModel",objRef.handleResponse, objRef);
-      }
-      objRef.transactionResponseModel.newRequest(objRef.transactionResponseModel,objRef.httpPayload);
-    }
+      selectBox: function (position) {
+        var bounds, minXY, maxXY;
+        if (position instanceof OpenLayers.Bounds) {
+        // it's a box
+          minXY = this.map.getLonLatFromPixel(
+            new OpenLayers.Pixel(position.left, position.bottom));
+          maxXY = this.map.getLonLatFromPixel(
+            new OpenLayers.Pixel(position.right, position.top));
+        } else {
+        // it's a pixel
+          minXY = this.map.getLonLatFromPixel(
+            new OpenLayers.Pixel(position.x-this.tolerance, position.y-this.tolerance));
+          maxXY = this.map.getLonLatFromPixel(
+            new OpenLayers.Pixel(position.x+this.tolerance, position.y+this.tolerance));
+        }
+        bounds = new OpenLayers.Bounds(minXY.lon, minXY.lat, maxXY.lon, maxXY.lat);
+        // now create request url
+        this.httpPayload.url = this.webServiceUrl+OpenLayers.Util.getParameterString({
+          SERVICE: "WFS",
+          VERSION: "1.0.0",
+          REQUEST: "GetFeature",
+          TYPENAME: this.typeName,
+          MAXFEATURES: this.maxFeatures,
+          BBOX: bounds.toBBOX()
+        });
+        this.trm.newRequest(this.trm, this.httpPayload);
+      },
+    });
+    return new Control();
   }
-  
-
-  /**
-   * Register for mouseUp events.
-   * @param objRef  Pointer to this object.
-   */
-  this.setMouseListener = function(objRef) {
-    if (objRef.mouseHandler) {
-      objRef.mouseHandler.model.addListener('mouseup',objRef.doAction,objRef);
-    }
-    objRef.context=objRef.widgetNode.selectSingleNode("mb:context");
-    if (objRef.context){
-      objRef.context=window.config.objects[objRef.context.firstChild.nodeValue];
-    }
-  }
-  config.addListener( "loadModel", this.setMouseListener, this );
 }
