2ae2471a8e5a65d085b30c64cce25a44b47ce6a2
ahocevar <ahocevar@78ec1247-0520-a048-8ee2-f9631b190d0c>
2007-05-14 06:59:10 +0000

    added capability to transform the model using a xsl stylesheet before passing the FeatureCollection to OpenLayers

--- sandbox/ol-integration/mapbuilder/lib/widget/GmlRendererOL.js
+++ sandbox/ol-integration/mapbuilder/lib/widget/GmlRendererOL.js
@@ -6,17 +6,17 @@ $Id$
 */
 
 // Ensure this object's dependancies are loaded.
-mapbuilder.loadScript(baseDir+"/model/Proj.js");
+mapbuilder.loadScript(baseDir+"/widget/WidgetBase.js");
 
 /**
  * Render GML into HTML.
- * Calls GmlCoordinates2Coord.xsl to convert GML to a simpler form.
- * Calls GmlRendererWZ.xsl to convert GML to wz_jsgraphics graphic function
- * calls.
- * this.targetModel references the context model with
- * width/height attributes.
+ * this.targetModel references the context model for the map
+ * where the content of this widget should be rendered to.
+ * If the model doc is not wfs compliant, a stylesheet
+ * property has to be set for this widget. The xsl file
+ * referenced in this property transforms the model doc to
+ * a wfs FeatureCollection.
  * @constructor
- * @base MapContainerBase
  * @param widgetNode  The widget's XML object node from the configuration document.
  * @param model       The model object that this widget belongs to.
  */
@@ -31,6 +31,14 @@ function GmlRendererOL(widgetNode, model) {
     this.model.tipWidgetId = tipWidget.firstChild.nodeValue;
   }
 
+  // Set this.stylesheet. This is taken from WidgetBaseXSL.js
+  if ( !this.stylesheet ) {
+    var styleNode = widgetNode.selectSingleNode("mb:stylesheet");
+    if (styleNode ) {
+      this.stylesheet = new XslProcessor(styleNode.firstChild.nodeValue,model.namespace);
+    }
+  }
+
   this.paint = function(objRef) {
     // remove and destroy layer
     if (objRef.olLayer) {
@@ -38,7 +46,9 @@ function GmlRendererOL(widgetNode, model) {
       objRef.olLayer.destroy();
       objRef.olLayer = null;
     }
-    var doc = objRef.model.doc;
+    // transform the model using the xsl stylesheet if there is one,
+    // otherwise just take the model doc.
+    var doc = objRef.stylesheet ? objRef.stylesheet.transformNodeToObject(objRef.model.doc) : objRef.model.doc;
     // nothing to do here if there is no model doc
     // or if the model doc contains an editing template
     if (!doc || objRef.model.getParam('isTemplate') == true) {
