4dfd062fdc06487e95894464e9623aaecb2d75fd
ahocevar <ahocevar@78ec1247-0520-a048-8ee2-f9631b190d0c>
2007-05-28 20:50:56 +0000

    modified example to use MapPaneOL; introduced new toggle button behaviour.

--- sandbox/ol-integration/mapbuilder/demo/mapViewer/GetFeatureInfo.js
+++ sandbox/ol-integration/mapbuilder/demo/mapViewer/GetFeatureInfo.js
@@ -19,50 +19,50 @@ mapbuilder.loadScript(baseDir+"/widget/ButtonBase.js");
 function GetFeatureInfo(widgetNode, model) {
   // Extend ButtonBase
   ButtonBase.apply(this, new Array(widgetNode, model));
-
+  
   /**
-   * Calls the centerAt method of the context doc to zoom out, recentering at 
-   * the mouse event coordinates.
-   * TBD: set the zoomBy property as a button property in conifg
-   * @param objRef      Pointer to this AoiMouseHandler object.
-   * @param targetNode  The node for the enclosing HTML tag for this widget.
+   * GetFeatureInfo control
+   * @param objRef reference to this object.
+   * @return {OpenLayers.Control} class of the OL control.
    */
-  this.doAction = function(objRef,targetNode) {
-    if (!objRef.enabled) return;
-    var layerNameList = new Array();
-    var selectedLayer=objRef.targetModel.getParam("selectedLayer");
-    if (selectedLayer==null) {
-      var queryList = objRef.targetModel.getQueryableLayers();
-      if (queryList.length==0) {
-         alert("There are no queryable layers available, please add a queryable layer to the map.");
-         return;
-      } else {
-        for (var i=0; i<queryList.length; ++i) {
-          layerNameList[i] = queryList[i].firstChild.nodeValue;   //convert to the layer names
+  this.createControl = function(objRef) {
+    var Control = OpenLayers.Class.create();
+    Control.prototype = OpenLayers.Class.inherit( OpenLayers.Control, {
+      CLASS_NAME: 'mbControl.GetFeatureInfo',
+      type: OpenLayers.Control.TYPE_TOOL,
+      objRef: objRef,
+      draw: function() {
+        this.handler = new OpenLayers.Handler.Box( this,
+          {done: this.zoomBox}, {keyMask: this.keyMask} );
+      },
+      zoomBox: function() {
+        var objRef = this.objRef
+        if (!objRef.enabled) return;
+        var layerNameList = new Array();
+        var selectedLayer=objRef.targetModel.getParam("selectedLayer");
+        if (selectedLayer==null) {
+          var queryList = objRef.targetModel.getQueryableLayers();
+          if (queryList.length==0) {
+             alert("There are no queryable layers available, please add a queryable layer to the map.");
+             return;
+          } else {
+            for (var i=0; i<queryList.length; ++i) {
+              layerNameList[i] = queryList[i].firstChild.nodeValue;   //convert to the layer names
+            }
+          }
+        } else {
+          layerNameList[0]= selectedLayer;
+        }
+        for (var i=0; i<layerNameList.length; ++i) {
+          var layerName = layerNameList[i];
+          var hidden = objRef.targetModel.getHidden(layerName);
+          if (hidden == 0) { //query only visible layers
+            config.objects.featureInfoController.requestStylesheet.setParameter("queryLayer", layerName);//TBD remove the hardcoded object ID here
+            objRef.targetModel.setParam("wms_GetFeatureInfo", layerName);
+          }
         }
       }
-    } else {
-      layerNameList[0]= selectedLayer;
-    }
-    for (var i=0; i<layerNameList.length; ++i) {
-      var layerName = layerNameList[i];
-      var hidden = objRef.targetModel.getHidden(layerName);
-      if (hidden == 0) { //query only visible layers
-        config.objects.featureInfoController.requestStylesheet.setParameter("queryLayer", layerName);//TBD remove the hardcoded object ID here
-        objRef.targetModel.setParam("wms_GetFeatureInfo", layerName);
-      }
-    }
+    });
+    return Control;
   }
-
-  /**
-   * Register for mouseUp events.
-   * @param objRef  Pointer to this object.
-   */
-  this.setMouseListener = function(objRef) {
-    if (objRef.mouseHandler) {
-      objRef.mouseHandler.model.addListener('mouseup',objRef.doAction,objRef);
-    }
-  }
-  this.model.addListener( "loadModel", this.setMouseListener, this );
-
 }
