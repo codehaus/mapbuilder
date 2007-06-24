286d3bcb0c72a35bd399e6e85fc2b3a7be8eeb49
ahocevar <ahocevar@78ec1247-0520-a048-8ee2-f9631b190d0c>
2007-06-24 19:41:42 +0000

    MAP-321: Added cleaned up functions from ModelSwitcher.js to CollectionList.js and let CollectionList.xsl produce HTML which uses them.

--- sandbox/ol-integration/mapbuilder/lib/widget/CollectionList.js
+++ sandbox/ol-integration/mapbuilder/lib/widget/CollectionList.js
@@ -11,6 +11,8 @@ mapbuilder.loadScript(baseDir+"/widget/WidgetBaseXSL.js");
 /**
  * A widget to display a list of context docs to pick from.  This is a view of 
  * a Context Collection as specified in the OGC Context specification.
+ * The default xsl stylesheet for this widget also uses the switchMap
+ * function, which allows to switch to a different map context.
  * @constructor
  * @base WidgetBase
  * @param widgetNode  The widget's XML object node from the configuration document.
@@ -20,4 +22,26 @@ mapbuilder.loadScript(baseDir+"/widget/WidgetBaseXSL.js");
 function CollectionList(widgetNode, model) {
   WidgetBaseXSL.apply(this,new Array(widgetNode, model));
 
+  /**
+   * Switch to another map context, keeping the current extent.
+   * @param objRef this widget
+   * @modelUrl the URL of the context we want to switch to.
+   */
+  this.switchMap = function(objRef, modelUrl) {
+    // save the current extent
+    objRef.extent = objRef.targetModel.map.getExtent();
+   
+    objRef.targetModel.addListener( "loadModel", objRef.setExtent, objRef );
+    config.loadModel( objRef.targetModel.id, modelUrl );
+  }
+  
+  /**
+   * Sets the extent of the map that we just switched to.
+   * Called when the map is reloaded with the new context.
+   * @param objRef this widget
+   */
+  this.setExtent = function (objRef) {
+    objRef.targetModel.removeListener( "loadModel", objRef.setExtent, objRef );
+    objRef.targetModel.map.zoomToExtent(objRef.extent);
+  }
 }
