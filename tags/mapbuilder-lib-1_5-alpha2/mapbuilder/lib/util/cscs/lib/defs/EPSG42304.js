2fc89d129401f77cfdf43565667b0894fd45c2c5
mattdiez <mattdiez@78ec1247-0520-a048-8ee2-f9631b190d0c>
2007-08-21 15:08:17 +0000

    Retagging alpha2

--- tags/mapbuilder-lib-1_5-alpha2/mapbuilder/lib/util/cscs/lib/defs/EPSG42304.js
+++
@@ -1,4 +0,0 @@
-//## NAD83 LCC for Canada
-//<42304> +proj=lcc +ellps=GRS80 +lat_0=49 +lon_0=-95 +lat_1=49 +lat_2=77 +datum=NAD83 +units=m no_defs <>
-
-csList.EPSG42304= "+title=NAD83 LCC for Canada EPSG:42304 +proj=lcc +ellps=GRS80 +lat_0=49 +lon_0=-95 +lat_1=49 +lat_2=77 +x_0=0 +y_0=0.0 +datum=NAD83 +units=m ";
