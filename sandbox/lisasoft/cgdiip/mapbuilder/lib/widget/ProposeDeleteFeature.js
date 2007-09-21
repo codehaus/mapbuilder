8110f72898cecf42f6303fb8a099b366ada36157
mvivian <mvivian@78ec1247-0520-a048-8ee2-f9631b190d0c>
2007-09-21 04:32:17 +0000

    Fixed a bug and added automatic updated time

--- sandbox/lisasoft/cgdiip/mapbuilder/lib/widget/ProposeDeleteFeature.js
+++ sandbox/lisasoft/cgdiip/mapbuilder/lib/widget/ProposeDeleteFeature.js
@@ -65,6 +65,7 @@ function ProposeDeleteFeature(widgetNode, model) {
       	point = objRef.getFirstPoint(objRef.targetModel);
         objRef.targetModel.setXpathValue(objRef.targetModel,"//georss:where/gml:Point/gml:pos",point);
       	objRef.targetModel.setXpathAttribute(objRef.targetModel,"//def:category[@scheme='http://www.geobase.ca/scheme/action']","term","Delete");
+      	objRef.targetModel.setXpathValue(objRef.targetModel,"//def:entry/def:updated",objRef.getISO8601Time());
       	s = objRef.targetModel.doc;
       	//s=objRef.cdataElementXsl.transformNodeToObject(s);
         s=objRef.insertXsl.transformNodeToObject(s);
@@ -124,7 +125,7 @@ function ProposeDeleteFeature(widgetNode, model) {
       coords = nodes[n].firstChild.nodeValue.trim().split(" ");
       if(coords.length >= 0 && coords[0]!= "")
       {
-        result = coords[0];
+        result = coords[0].replace(","," ");
       }
       else
       {
@@ -160,5 +161,51 @@ function ProposeDeleteFeature(widgetNode, model) {
         objRef.targetContext.callListeners("refreshWmsLayers");
       }
     }
+  }
+  
+  this.getISO8601Time=function() {
+	var today = new Date();
+	var year = today.getYear();
+	if (year < 2000) // Y2K Fix, Isaac Powell
+	year = year + 1900; // http://onyx.idbsu.edu/~ipowell
+	var month = today.getMonth() + 1;
+	var day = today.getDate();
+	var hour = today.getHours();
+	var hourUTC = today.getUTCHours();
+	var diff = hour - hourUTC;
+	var hourdifference = Math.abs(diff);
+	var minute = today.getMinutes();
+	var minuteUTC = today.getUTCMinutes();
+	var minutedifference;
+	var second = today.getSeconds();
+	var timezone;
+	if (minute != minuteUTC && minuteUTC < 30 && diff < 0) { hourdifference--; }
+	if (minute != minuteUTC && minuteUTC > 30 && diff > 0) { hourdifference--; }
+	if (minute != minuteUTC) {
+		minutedifference = ":30";
+	}
+	else {
+		minutedifference = ":00";
+	}
+	if (hourdifference < 10) {
+		timezone = "0" + hourdifference + minutedifference;
+	}
+	else {
+		timezone = "" + hourdifference + minutedifference;
+	}
+	if (diff < 0) {
+		timezone = "-" + timezone;
+	}
+	else {
+		timezone = "+" + timezone;
+	}
+	if (month <= 9) month = "0" + month;
+	if (day <= 9) day = "0" + day;
+	if (hour <= 9) hour = "0" + hour;
+	if (minute <= 9) minute = "0" + minute;
+	if (second <= 9) second = "0" + second;
+	time = year + "-" + month + "-" + day + "T"
+	+ hour + ":" + minute + ":" + second + timezone;
+	return time;
   }
 }
--- sandbox/lisasoft/cgdiip/mapbuilder/lib/widget/ProposeInsertFeature.js
+++ sandbox/lisasoft/cgdiip/mapbuilder/lib/widget/ProposeInsertFeature.js
@@ -81,14 +81,14 @@ function ProposeInsertFeature(widgetNode, model) {
         
         point = objRef.getFirstPoint(objRef.targetModel);
         objRef.targetModel.setXpathValue(objRef.targetModel,"//georss:where/gml:Point/gml:pos",point);
+        
+        objRef.targetModel.setXpathValue(objRef.targetModel,"//def:entry/def:updated",objRef.getISO8601Time());
 	    
 	    s = objRef.targetModel.doc;
         //s=objRef.cdataElementXsl.transformNodeToObject(s);
         
         //mvivian: Will always be inserting proposed changes
         s=objRef.insertXsl.transformNodeToObject(s);
-        
-        
         
         objRef.httpPayload.postData=s;
         objRef.transactionResponseModel.transactionType="insert";
@@ -145,7 +145,7 @@ function ProposeInsertFeature(widgetNode, model) {
       coords = nodes[n].firstChild.nodeValue.trim().split(" ");
       if(coords.length >= 0 && coords[0]!= "")
       {
-        result = coords[0];
+        result = coords[0].replace(",", " ");
       }
       else
       {
@@ -181,5 +181,51 @@ function ProposeInsertFeature(widgetNode, model) {
         objRef.targetContext.callListeners("refreshWmsLayers");
       }
     }
+  }
+  
+  this.getISO8601Time=function() {
+	var today = new Date();
+	var year = today.getYear();
+	if (year < 2000) // Y2K Fix, Isaac Powell
+	year = year + 1900; // http://onyx.idbsu.edu/~ipowell
+	var month = today.getMonth() + 1;
+	var day = today.getDate();
+	var hour = today.getHours();
+	var hourUTC = today.getUTCHours();
+	var diff = hour - hourUTC;
+	var hourdifference = Math.abs(diff);
+	var minute = today.getMinutes();
+	var minuteUTC = today.getUTCMinutes();
+	var minutedifference;
+	var second = today.getSeconds();
+	var timezone;
+	if (minute != minuteUTC && minuteUTC < 30 && diff < 0) { hourdifference--; }
+	if (minute != minuteUTC && minuteUTC > 30 && diff > 0) { hourdifference--; }
+	if (minute != minuteUTC) {
+		minutedifference = ":30";
+	}
+	else {
+		minutedifference = ":00";
+	}
+	if (hourdifference < 10) {
+		timezone = "0" + hourdifference + minutedifference;
+	}
+	else {
+		timezone = "" + hourdifference + minutedifference;
+	}
+	if (diff < 0) {
+		timezone = "-" + timezone;
+	}
+	else {
+		timezone = "+" + timezone;
+	}
+	if (month <= 9) month = "0" + month;
+	if (day <= 9) day = "0" + day;
+	if (hour <= 9) hour = "0" + hour;
+	if (minute <= 9) minute = "0" + minute;
+	if (second <= 9) second = "0" + second;
+	time = year + "-" + month + "-" + day + "T"
+	+ hour + ":" + minute + ":" + second + timezone;
+	return time;
   }
 }
