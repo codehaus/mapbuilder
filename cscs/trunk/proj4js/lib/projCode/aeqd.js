deda041a261227892f6fbfe112315fcf0d1e9a4a
madair <madair@78ec1247-0520-a048-8ee2-f9631b190d0c>
2007-10-08 21:09:57 +0000

    adding in OL build scripts and tools, directory renaming, cleanup of old code

---
+++ cscs/trunk/proj4js/lib/projCode/aeqd.js
@@ -0,0 +1,84 @@
+Proj4js.Proj.aeqd = {
+  init : function() {
+    this.sin_p12=Math.sin(this.lat0)
+    this.cos_p12=Math.cos(this.lat0)
+  },
+
+  forward: function(p) {
+    var lon=p.x;
+    var lat=p.y;
+
+    var sinphi=Math.sin(p.y);
+    var cosphi=Math.cos(p.y); 
+    var dlon = Proj4js.common.adjust_lon(lon -this.long0);
+    var coslon = Math.cos(dlon);
+    var g = this.sin_p12 * sinphi + this.cos_p12 * cosphi * coslon;
+    if (Math.abs(Math.abs(g) - 1.0) < Proj4js.common.EPSLN)
+       {
+       var ksp = 1.0;
+       if (g < 0.0)
+         {
+       var  con = 2.0 * Proj4js.common.HALF_PI * this.a;
+            if (!MB_IGNORE_CSCS_ERRORS) alert(mbGetMessage("aeqdFwdPointError"));
+            }
+       }
+    else
+       {
+       var z = Math.acos(g);
+        ksp = z/ Math.sin(z);
+       }
+    var x = this.x0+this. a * ksp * cosphi * Math.sin(dlon);
+    var y = this.y0 + this.a * ksp * (this.cos_p12 * sinphi - this.sin_p12 * cosphi * coslon);
+    return new Proj4js.Point(x, y);
+  },
+
+  inverse: function(p){
+    p.x -= this.x0;
+    p.y -= this.y0;
+
+    var rh = Math.sqrt(p.x * p.x + p.y *p. y);
+    if (rh > (2.0 * Proj4js.common.HALF_PI * this.a))
+       {
+       if (!MB_IGNORE_CSCS_ERRORS) alert(mbGetMessage("aeqdInvDataError"));
+
+
+       }
+    var z = rh / this.a;
+
+    var sinz=Math.sin(z)
+    var cosz=Math.cos(z)
+
+    var lon = this.long0;
+    if (Math.abs(rh) <= EPSLN)
+       {
+       var lat = this.lat0;
+
+       }
+    lat = asinz(cosz * this.sin_p12 + (p.y * sinz * this.cos_p12) / rh);
+    this.t3=lat;
+    var con = Math.abs(this.lat0) - Proj4js.common.HALF_PI;
+    if (Math.abs(con) <= Proj4js.common.EPSLN)
+       {
+       if (lat0 >= 0.0)
+          {
+          lon = adjust_lon(this.long0 + Math.atan2(p.x , -p.y));
+
+          }
+       else
+          {
+         lon = adjust_lon(this.long0 - Math.atan2(-p.x , p.y));
+
+          }
+       }
+
+    con = cosz - this.sin_p12 * Math.sin(lat);
+    if ((Math.abs(con) < Proj4js.common.EPSLN) && (Math.abs(x) < Proj4js.common.EPSLN))
+       return(19);
+    this.t1= this.cos_p12;
+    this.t2=con * rh;
+    this. temp = Math.atan2((p.x * sinz * this.cos_p12), (con * rh));
+    lon = Proj4js.common.adjust_lon(this.long0 + Math.atan2((p.x * sinz * this.cos_p12), (con * rh)));
+
+    return new Proj4js.Point(lon, lat);
+  } 
+};
---
+++ cscs/trunk/proj4js/lib/projCode/gauss.js
@@ -0,0 +1,56 @@
+
+Proj4js.Proj.gauss = {
+
+  init : function() {
+    sphi = Math.sin(this.lat0);
+    cphi = Math.cos(this.lat0);  
+    cphi *= cphi;
+    this.rc = Math.sqrt(1.0 - this.es) / (1.0 - this.es * sphi * sphi);
+    this.C = Math.sqrt(1.0 + this.es * cphi * cphi / (1.0 - this.es));
+    this.phic0 = Math.asin(sphi / this.C);
+    this.ratexp = 0.5 * this.C * this.e;
+    this.K = Math.tan(0.5 * this.phic0 + Proj4js.common.FORTPI) / (Math.pow(Math.tan(0.5*this.lat0 + Proj4js.common.FORTPI), this.C) * Proj4js.common.srat(this.e*sphi, this.ratexp));
+  },
+
+  forward : function(p) {
+    var lon = p.x;
+    var lat = p.y;
+    // convert to radians
+    lon = lon * Proj4js.common.D2R;
+    lat = lat * Proj4js.common.D2R;
+
+    lon = Proj4js.common.adjust_lon(lon-this.long0); /* adjust del longitude */
+    p.y = 2.0 * Math.atan( this.K * Math.pow(Math.tan(0.5 * lat + Proj4js.common.FORTPI), this.C) * Proj4js.common.srat(this.e * Math.sin(lat), this.ratexp) ) - Proj4js.common.HALF_PI;
+    p.x = this.C * lon;
+    //p.x = this.a * p.x + this.x0;
+    //p.y = this.a * p.y + this.y0;
+    return p;
+  },
+
+  inverse : function(p) {
+    //p.x = (p.x - this.x0) * this.a; /* descale and de-offset */
+    //p.y = (p.y - this.y0) * this.a;
+
+    var DEL_TOL = 1e-14;
+    p.x = p.x / this.C;
+    var lat = p.y;
+    num = Math.pow(Math.tan(0.5 * p.y + Proj4js.common.FORTPI)/this.K, 1./this.C);
+    for (var i = Proj4js.common.MAX_ITER; i>0; --i) {
+      lat = 2.0 * Math.atan(num * Proj4js.common.srat(this.e * Math.sin(p.y), -0.5 * this.e)) - Proj4js.common.HALF_PI;
+      if (Math.abs(lat - p.y) < DEL_TOL) break;
+      p.y = lat;
+    }	
+    /* convergence failed */
+    if (!i) {
+      Proj4js.reportError("gauss:inverse:convergence failed");
+      return null;
+    }
+
+    p.x = Proj4js.common.adjust_lon(p.x + this.long0); /* adjust longitude to CM */
+
+    p.x = p.x*Proj4js.common.R2D;
+    p.y = lat*Proj4js.common.R2D;
+    return p;
+  }
+};
+
