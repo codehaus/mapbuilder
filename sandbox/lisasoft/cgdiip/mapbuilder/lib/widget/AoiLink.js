3ba1db786960be2795895a14f73d496cde1abadc
mvivian <mvivian@78ec1247-0520-a048-8ee2-f9631b190d0c>
2007-11-02 02:27:13 +0000

    IPP-90 Three clients link together and take the Area of Intrest with them.

---
+++ sandbox/lisasoft/cgdiip/mapbuilder/lib/widget/AoiLink.js
@@ -0,0 +1,49 @@
+/*
+License: LGPL as per: http://www.gnu.org/copyleft/lesser.html
+$Id: ModelTransformer.js 3150 2007-08-20 22:50:33Z mvivian $
+*/
+
+// Ensure this object's dependancies are loaded.
+mapbuilder.loadScript(baseDir+"/widget/WidgetBase.js");
+
+/**
+ */
+function AoiLink(widgetNode, model) {
+  // Extend WidgetBase
+  WidgetBase.apply(this, new Array(widgetNode, model));
+  
+  this.href = widgetNode.selectSingleNode("mb:href").firstChild.nodeValue;
+  this.linkLabel = widgetNode.selectSingleNode("mb:linkLabel").firstChild.nodeValue;
+  this.bbox = "";
+  
+  this.drawLink = function(objRef)
+  {
+  	if(objRef.htmlTagId){
+  	  var href = objRef.href
+  	  if(href.contains('?'))
+  	  	href = href+'&bbox=';
+  	  else
+  	    href = href+'?bbox=';
+  	    
+  	  var html = "<a href=\"" + href + objRef.bbox + "\">" + objRef.linkLabel + "</a>";
+  	  document.getElementById(objRef.htmlTagId).innerHTML = html;
+  	}
+  	
+  }
+  
+  this.init = function(objRef) {
+  	extent = objRef.model.getBoundingBox();
+  	objRef.bbox = extent[0] + "," + extent[1] + "," + extent[2] + "," + extent[3];
+  	objRef.drawLink(objRef);
+    objRef.model.map.events.register('moveend', objRef, objRef.bboxHandler);
+  }
+  
+  this.model.addListener("loadModel", this.init, this );
+
+  this.bboxHandler = function(objRef) {
+    var extent=objRef.object.getExtent();
+
+    this.bbox = extent.left + "," + extent.bottom + "," + extent.right + "," + extent.top;
+    this.drawLink(this);
+  }
+}
