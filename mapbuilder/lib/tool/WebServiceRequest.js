f24d73d74ce3c3a97e4507ac2bfa826e59f49731
ahocevar <ahocevar@78ec1247-0520-a048-8ee2-f9631b190d0c>
2007-11-23 17:47:32 +0000

    restored MapServiceRequest to not delete any templates; instead, re-configured the mapViewer example to not use a template model for the featureInfo container

--- mapbuilder/lib/tool/WebServiceRequest.js
+++ mapbuilder/lib/tool/WebServiceRequest.js
@@ -117,7 +117,6 @@ WebServiceRequest.prototype.doRequest = function(objRef, featureName) {
   }
   if (objRef.model.setRequestParameters) objRef.model.setRequestParameters(featureName, objRef.requestStylesheet);
   var httpPayload = objRef.createHttpPayload(feature);
-  objRef.targetModel.deleteTemplates();
   objRef.targetModel.newRequest(objRef.targetModel,httpPayload);
 }
 
