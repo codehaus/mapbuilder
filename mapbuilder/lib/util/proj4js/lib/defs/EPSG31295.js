451b6ae2f3a1a347da514facbbcc612e9751c22a
ahocevar <ahocevar@78ec1247-0520-a048-8ee2-f9631b190d0c>
2007-12-05 23:43:22 +0000

    finished move of defs from lib to proj4js (MAP-443)

--- mapbuilder/lib/util/proj4js/lib/defs/EPSG31295.js
+++
@@ -1,3 +0,0 @@
-//# MGI / M31
-//<31295> +proj=tmerc +lat_0=0 +lon_0=13.33333333333333 +k=1.000000 +x_0=450000 +y_0=0 +ellps=bessel +units=m +no_defs  <>
-Proj4js.defs["EPSG:31295"] = "+title=MGI / M31 EPSG:31295 +proj=tmerc +lat_0=0 +lon_0=13.33333333333333 +k=1.000000 +x_0=450000 +y_0=0 +ellps=bessel +units=m";
