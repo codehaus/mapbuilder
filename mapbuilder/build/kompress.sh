b3670fea440f6bcc95127a74ad57dd339e5e18cc
ahocevar <ahocevar@78ec1247-0520-a048-8ee2-f9631b190d0c>
2007-12-19 21:47:32 +0000

    Merging r3739 to HEAD from trunk

--- mapbuilder/build/kompress.sh
+++ mapbuilder/build/kompress.sh
@@ -18,93 +18,121 @@ done;
 rm ${targetDir}/MapbuilderCompressed.js
 
 originalFile=${targetDir}/lib/MapbuilderCompressed.js
-outputFile=${targetDir}/lib/MapbuilderCompressedFixed.js
 
 # Compress javascript files
-java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/Mapbuilder.js > ${originalFile} 2>&1
-java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/RELEASE.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/RELEASE.js > ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/Mapbuilder.js >> ${originalFile} 2>&1
+
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/util/openlayers/OpenLayers.js >> ${originalFile} 2>&1
+
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/util/sarissa/Sarissa.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/util/sarissa/javeline_xpath.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/util/sarissa/javeline_xslt.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/util/sarissa/sarissa_dhtml.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/util/sarissa/sarissa_ieemu_xpath.js >> ${originalFile} 2>&1
+
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/util/proj4js/proj4js-compressed.js >> ${originalFile} 2>&1
+
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/util/Util.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/util/Listener.js >> ${originalFile} 2>&1
+
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/model/ModelBase.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/model/Config.js >> ${originalFile} 2>&1
 
 java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/ButtonBase.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/Button.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/Back.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/Forward.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/ZoomIn.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/ZoomOut.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/DragPan.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/Reset.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/MapPaneOL.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/CursorTrack.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/MapScaleText.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/Loading2.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/PanZoomBar.js >> ${originalFile} 2>&1
+
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/EditButtonBase.js >> ${originalFile} 2>&1
 java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/EditLine.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/EditPoint.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/EditPolygon.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/InsertFeature.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/DeleteFeature.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/WfsGetFeature.js >> ${originalFile} 2>&1
+
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/Legend.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/Version.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/OverviewMap.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/TransactionResponse.js >> ${originalFile} 2>&1
+
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/model/Model.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/model/Context.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/model/OwsContext.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/model/Transaction.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/model/FeatureCollection.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/model/StyledLayerDescriptor.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/model/WfsCapabilities.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/model/WmsCapabilities.js >> ${originalFile} 2>&1
+
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/Loading.js >> ${originalFile} 2>&1
 
 
-for file in `find ${targetDir}/lib -name "*.js" | \
-	 egrep -v "overlib" \
-	| egrep -v "/lib/Mapbuilder.js" \
-	| egrep -v "/lib/RELEASE.js" \
-	| egrep -v "/lib/widget/ButtonBase.js" \
-	| egrep -v "/lib/widget/EditLine.js" \
-	| egrep -v "/lib/MapbuilderServerLoad.js" \
-	| egrep -v "/lib/MapbuilderCompressed.js"`; do
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/WidgetBase.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/WidgetBaseXSL.js >> ${originalFile} 2>&1
 
-#  ${mapbuilderDir}/build/jsjam.pl ${file} -i -g -n > ${tmp};
-  echo "Compressing ${file}";
-  java -jar lib/util/custom_rhino.jar -c ${file} >> ${originalFile} 2>&1
-done;
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/TipWidgetBase.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/TipWidgetConfig.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/TipWidgetOL.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/GmlRendererBase.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/GmlRendererConfig.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/GmlRendererOL.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/FeatureList.js >> ${originalFile} 2>&1
 
-# Step 1
-# change "stylesheet.setParameter("objref","objRef") to "stylesheet.setParameter(_var,"objRef")"
-# in GmlRendererWZ.js
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/tool/AoiMouseHandler.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/tool/Caps2Context.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/tool/ToolBase.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/tool/EditContext.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/tool/Extent.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/tool/History.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/tool/FeatureSelectHandler.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/tool/MergeModels.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/tool/MovieLoop.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/tool/WebServiceRequest.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/tool/ZoomToAoi.js >> ${originalFile} 2>&1
 
-cat ${originalFile}  | sed 's/\(_[0-9]\)\(.stylesheet.setParameter(\"objRef\",\)\(\"objRef\"\)/\1\2\1/' > ${outputFile}
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/GetFeatureInfoWSR.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/Graticule.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/MapScaleBar.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/LegendGraphic.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/Abstract.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/FeatureInfo.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/MapTitle.js >> ${originalFile} 2>&1
 
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/SetAoi.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/ModelUrlInput.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/CollectionList.js >> ${originalFile} 2>&1
 
-# Step 2
-# block the loadscript function
-# Original Code:
-#  var _3=document.createElement("script");
-# _3.defer=false;
-# _3.type="text/javascript";
-# _3.src=_2;
-# _3.id=_2;
-# document.getElementsByTagName("head")[0].appendChild(_3);
-# this.loadingScripts.push(_3);
-# }
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/SelectMapLayers.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/Timestamp.js >> ${originalFile} 2>&1
 
-# Revised Code:
-#  var _3=document.createElement("script");
-#  _3.readyState=="complete";
-#  this.loadingScripts.push(_3);
-#  }
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/Save.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/LayerControl.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/AoiBoxDHTML.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/FilterAttributes.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/SelectFeatureType.js >> ${originalFile} 2>&1
 
-cat ${originalFile} | tr '\n' '\t' | sed  's/\(_[0-9]\)\(\=document.createElement(\"script");\).*\(this.loadingScripts.push\)/\1\2\t\1.readyState=="complete";\t\3/' | tr '\t' '\n' > ${outputFile}
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/AoiForm.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/Widget.js >> ${originalFile} 2>&1
 
-mv ${outputFile} ${originalFile}
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/WebServiceForm.js >> ${originalFile} 2>&1
 
-# Step 3
-# 
-# Loadscript has been blocked, but IE is still stupid
-#
-# Add: ||this.loadingScripts[0].readyState=="uninitialized" (to mapbuilder load state loop)
-# 
-# Original Code: 
-#  while(this.loadingScripts.length>0&&(this.loadingScripts[0].readyState=="loaded"||this.loadingScripts[0].read yState=="complete"||this.loadingScripts[0].readyState==null)){
-#
-# Revised Code:
-# while(this.loadingScripts.length>0&&(this.loadingScripts[0].readyState=="loaded"||this.loadingScripts[0].read
-# yState=="uninitialized"||this.loadingScripts[0].readyState=="complete"||this.loadingScripts[0].readyState==null)){
-
-
-cat ${originalFile}  | sed 's/\(this.loadingScripts\[0\].readyState==\"loaded\"\)/\1||this.loadingScripts[0].readyState=="uninitialized"/' > ${outputFile}
-
-mv ${outputFile} ${originalFile}
-
-# Step 4
-#
-# "disabledImage" gets set to some var (often _5)
-#
-# This will set it back
-
-# cat ${outputFile} | tr '\n' '\t' | \
-# sed  '/function ButtonBase/,/function ToolBase/ s/\(var \)\(_[0-9]\)\(\=_[0-9].selectSingleNode(\"mb:disabledSrc\");\)/\1disabledImage\3disabledImage/' \
-# | tr '\t' '\n' > ${outputFile}
-
-# TODO - FIX THIS, THIS IS A HACK
-# temporarily disabling
-
-
-cat ${originalFile}  | sed '/function ButtonBase/,/function EditLine/ {s/_5/disabledImage/}' > ${outputFile} 
-
-mv ${outputFile} ${originalFile}
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/GetFeatureInfo.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/Locations.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/Measurement.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/ModelStatus.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/SaveModel.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/ShowDistance.js >> ${originalFile} 2>&1
+java -jar lib/util/custom_rhino.jar -c ${targetDir}/lib/widget/SelectAllMapLayers.js >> ${originalFile} 2>&1
 
 cp ${originalFile} ${mapbuilderDir}/lib
