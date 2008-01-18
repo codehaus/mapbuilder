08ccb7d92b79b574dce7222a1a6209fff2c21c84
ahocevar <ahocevar@78ec1247-0520-a048-8ee2-f9631b190d0c>
2008-01-18 13:57:21 +0000

    Option for editing buttons to choose if a new empty feature should be added to the model when editing is finished. (closes MAP-479). Along with that, refactored editing buttons to use OpenLayers.Format.GML().write to create the geometries instead of doing this "by hand". As a result, EditPoint.js, EditLine.js and EditPolygon.js could be significantly simplified, while more editing functionality is now centralized in EditButtonBase.js

--- mapbuilder/lib/widget/EditLine.js
+++ mapbuilder/lib/widget/EditLine.js
@@ -18,45 +18,8 @@ mapbuilder.loadScript(baseDir+"/widget/EditButtonBase.js");
 function EditLine(widgetNode, model) {
   // Extend EditButtonBase
   EditButtonBase.apply(this, new Array(widgetNode, model));
-
-  /**
-   * Interactive EditLine control.
-   * @param objRef reference to this object.
-   * @return {OpenLayers.Control} class of the OL control.
-   */
-  this.createControl = function(objRef) {
-    var Control = OpenLayers.Class(OpenLayers.Control.DrawFeature, {
-    	type: OpenLayers.Control.TYPE_TOOL,
-      // this is needed because all editing tools are of type
-      // OpenLayers.Control.DrawFeature
-      CLASS_NAME: 'mbEditLine'
-    });
-    return Control;
-  }
   
   this.instantiateControl = function(objRef, Control) {
     return new Control(objRef.featureLayer, OpenLayers.Handler.Path);
-  }
-
-  /**
-   * Append a line to the enclosing GML model.
-   * @param objRef      Pointer to this object.
-   */
-  this.setFeature = function(objRef) {
-    if (objRef.enabled && objRef.geometry) {
-      var points = objRef.geometry.components;
-      var geom = '';
-      for (var i in points) {
-        geom += ' '+points[i].x+","+points[i].y;
-      }
-      sucess=objRef.targetModel.setXpathValue(
-        objRef.targetModel,
-        objRef.featureXpath,
-        geom);
-      objRef.geometry = null;
-      if(!sucess){
-        alert(mbGetMessage("invalidFeatureXpathEditLine", objRef.featureXpath));
-      }
-    }
   }
 }
