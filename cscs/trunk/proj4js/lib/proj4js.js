19d8717e82e953112005efdb07f244ccfc22c278
ahocevar <ahocevar@78ec1247-0520-a048-8ee2-f9631b190d0c>
2008-02-16 01:03:01 +0000

    performance improvement for Spherical Mercator workaround check

--- cscs/trunk/proj4js/lib/proj4js.js
+++ cscs/trunk/proj4js/lib/proj4js.js
@@ -81,8 +81,8 @@ Proj4js = {
         }
         
         // Workaround for Spherical Mercator
-        if ((source.srsCode.match(/GOOGLE|900913/) && target.datum != "wgs84") ||
-            (dest.srsCode.match(/GOOGLE|900913/) && source.datum != "wgs84")) {
+        if ((source.srsProjNumber =="900913" && target.datum != "wgs84") ||
+            (dest.srsProjNumber == "900913" && source.datum != "wgs84")) {
             var wgs84 = new Proj4js.Proj("EPSG:4326");
             this.transform(source, wgs84, point);
             point.transformed = false;
