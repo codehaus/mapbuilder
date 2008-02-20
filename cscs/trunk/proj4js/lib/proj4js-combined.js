41cfa09cf8dc16020b66450c3431abce48a745df
ahocevar <ahocevar@78ec1247-0520-a048-8ee2-f9631b190d0c>
2008-02-20 09:30:31 +0000

    another performance improvement to the Spherical Mercator workaround; also created a new combined and compressed build.

--- cscs/trunk/proj4js/lib/proj4js-combined.js
+++ cscs/trunk/proj4js/lib/proj4js-combined.js
@@ -92,6 +92,15 @@ Proj4js = {
           this.log("point already transformed");
           return;
         }
+        
+        // Workaround for Spherical Mercator
+        if ((source.srsProjNumber =="900913" && dest.datumCode != "WGS84") ||
+            (dest.srsProjNumber == "900913" && source.datumCode != "WGS84")) {
+            var wgs84 = Proj4js.WGS84;
+            this.transform(source, wgs84, point);
+            point.transformed = false;
+            source = wgs84;
+        }
 
         // Transform source points to long/lat, if they aren't already.
         if ( source.projName=="longlat") {
@@ -1105,6 +1114,66 @@ Proj4js.Datum = {
 Proj4js.WGS84 = new Proj4js.Proj('WGS84');
 Proj4js.Datum['OSB36'] = Proj4js.Datum['OSGB36']; //as returned from spatialreference.org
 /* ======================================================================
+    projCode/sterea.js
+   ====================================================================== */
+
+
+Proj4js.Proj.sterea = {
+  dependsOn : 'gauss',
+
+  init : function() {
+    Proj4js.Proj['gauss'].init.apply(this);
+    if (!this.rc) {
+      Proj4js.reportError("sterea:init:E_ERROR_0");
+      return;
+    }
+    this.sinc0 = Math.sin(this.phic0);
+    this.cosc0 = Math.cos(this.phic0);
+    this.R2 = 2.0 * this.rc;
+    if (!this.title) this.title = "Oblique Stereographic Alternative";
+  },
+
+  forward : function(p) {
+    p.x = Proj4js.common.adjust_lon(p.x-this.long0); /* adjust del longitude */
+    Proj4js.Proj['gauss'].forward.apply(this, [p]);
+    sinc = Math.sin(p.y);
+    cosc = Math.cos(p.y);
+    cosl = Math.cos(p.x);
+    k = this.k0 * this.R2 / (1.0 + this.sinc0 * sinc + this.cosc0 * cosc * cosl);
+    p.x = k * cosc * Math.sin(p.x);
+    p.y = k * (this.cosc0 * sinc - this.sinc0 * cosc * cosl);
+    p.x = this.a * p.x + this.x0;
+    p.y = this.a * p.y + this.y0;
+    return p;
+  },
+
+  inverse : function(p) {
+    var lon,lat;
+    p.x = (p.x - this.x0) / this.a; /* descale and de-offset */
+    p.y = (p.y - this.y0) / this.a;
+
+    p.x /= this.k0;
+    p.y /= this.k0;
+    if ( (rho = Math.sqrt(p.x*p.x + p.y*p.y)) ) {
+      c = 2.0 * Math.atan2(rho, this.R2);
+      sinc = Math.sin(c);
+      cosc = Math.cos(c);
+      lat = Math.asin(cosc * this.sinc0 + p.y * sinc * this.cosc0 / rho);
+      lon = Math.atan2(p.x * sinc, rho * this.cosc0 * cosc - p.y * this.sinc0 * sinc);
+    } else {
+      lat = this.phic0;
+      lon = 0.;
+    }
+
+    p.x = lon;
+    p.y = lat;
+    Proj4js.Proj['gauss'].inverse.apply(this,[p]);
+    p.x = Proj4js.common.adjust_lon(p.x + this.long0); /* adjust longitude to CM */
+    return p;
+  }
+};
+
+/* ======================================================================
     projCode/aea.js
    ====================================================================== */
 
@@ -1256,66 +1325,6 @@ Proj4js.Proj.aea = {
 };
 
 
-
-/* ======================================================================
-    projCode/sterea.js
-   ====================================================================== */
-
-
-Proj4js.Proj.sterea = {
-  dependsOn : 'gauss',
-
-  init : function() {
-    Proj4js.Proj['gauss'].init.apply(this);
-    if (!this.rc) {
-      Proj4js.reportError("sterea:init:E_ERROR_0");
-      return;
-    }
-    this.sinc0 = Math.sin(this.phic0);
-    this.cosc0 = Math.cos(this.phic0);
-    this.R2 = 2.0 * this.rc;
-    if (!this.title) this.title = "Oblique Stereographic Alternative";
-  },
-
-  forward : function(p) {
-    p.x = Proj4js.common.adjust_lon(p.x-this.long0); /* adjust del longitude */
-    Proj4js.Proj['gauss'].forward.apply(this, [p]);
-    sinc = Math.sin(p.y);
-    cosc = Math.cos(p.y);
-    cosl = Math.cos(p.x);
-    k = this.k0 * this.R2 / (1.0 + this.sinc0 * sinc + this.cosc0 * cosc * cosl);
-    p.x = k * cosc * Math.sin(p.x);
-    p.y = k * (this.cosc0 * sinc - this.sinc0 * cosc * cosl);
-    p.x = this.a * p.x + this.x0;
-    p.y = this.a * p.y + this.y0;
-    return p;
-  },
-
-  inverse : function(p) {
-    var lon,lat;
-    p.x = (p.x - this.x0) / this.a; /* descale and de-offset */
-    p.y = (p.y - this.y0) / this.a;
-
-    p.x /= this.k0;
-    p.y /= this.k0;
-    if ( (rho = Math.sqrt(p.x*p.x + p.y*p.y)) ) {
-      c = 2.0 * Math.atan2(rho, this.R2);
-      sinc = Math.sin(c);
-      cosc = Math.cos(c);
-      lat = Math.asin(cosc * this.sinc0 + p.y * sinc * this.cosc0 / rho);
-      lon = Math.atan2(p.x * sinc, rho * this.cosc0 * cosc - p.y * this.sinc0 * sinc);
-    } else {
-      lat = this.phic0;
-      lon = 0.;
-    }
-
-    p.x = lon;
-    p.y = lat;
-    Proj4js.Proj['gauss'].inverse.apply(this,[p]);
-    p.x = Proj4js.common.adjust_lon(p.x + this.long0); /* adjust longitude to CM */
-    return p;
-  }
-};
 
 /* ======================================================================
     projCode/poly.js
